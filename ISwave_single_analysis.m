3fd4670eedaaf9961d80626799357b309ae9d968
Ilario Gelmetti <iochesonome@gmail.com>
2017-12-05 12:16:03 +0000

    ISwave_single_analysis added Jidrift

--- ISwave_single_analysis.m
+++ ISwave_single_analysis.m
@@ -1,4 +1,4 @@
-function [fit_coeff, subtracting_n_t, subtracting_n_intr_t, subtracting_n_contacts_t, subtracting_a_t] = ISwave_single_analysis(sol_i_Int_ISwave)
+function [fit_coeff, fit_idrift_coeff, subtracting_n_t, subtracting_n_intr_t, subtracting_n_contacts_t, subtracting_a_t] = ISwave_single_analysis(sol_i_Int_ISwave)
 % calculate impedance (reactance and resistance) and phase by impedance
 % spectroscopy with oscillating voltage
 
@@ -16,8 +16,24 @@ periods = round(s.params.tmax * s.params.Vapp_params(4) / (2 * pi));
 % round should not be needed here
 fit_t_index = round((s.params.tpoints - 1) * (periods - 1) / periods);
 fit_t = s.t(fit_t_index:end);
-fit_J = s.Jtotr(fit_t_index:end);
-fit_coeff = ISwave_single_fit(fit_t, fit_J, s.params.Vapp_func);
+fit_J = s.Jtotr(fit_t_index:end) / 1000; % in Ampere
+fit_Jidrift = s.Jidrift_points(fit_t_index:end, end) / 1000; % in Ampere
+fit_coeff = ISwave_single_fit(fit_t, fit_J, s.params.J_func);
+if isfield(s.params, 'Jpoints') % if the ionic drift has been calculated, fit it
+    fit_idrift_coeff = ISwave_single_fit(fit_t, fit_Jidrift, s.params.J_func);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% calculate current out of phase
+
+% calculate amplitude of out of phase oscillation
+J_amp_outphase = fit_coeff(2) * sin(fit_coeff(3));
+
+% calculate bias of out of phase component as fraction of total J_bias
+J_bias = fit_coeff(1);
+J_amp = fit_coeff(2);
+J_bias_outphase = J_bias * J_amp_outphase / J_amp;
+J_outphase = s.params.J_func([J_bias_outphase, J_amp_outphase, pi/2], fit_t);
 
 % fourth value of Vapp_params is pulsatance
 figure('Name', ['Single ISwave, Int ' num2str(s.params.Int) ' Freq ' num2str(s.params.Vapp_params(4) / (2 * pi))], 'NumberTitle', 'off');
@@ -28,22 +44,24 @@ figure('Name', ['Single ISwave, Int ' num2str(s.params.Int) ' Freq ' num2str(s.p
     
     yyaxis left
     hold off
-    plot(s.t, s.Jtotr);
+    plot(s.t, s.Jtotr / 1000); % Ampere
     hold on
 %     plot(s.t, s.Jpartr);
-    plot(s.t, s.Jdispr);
+    plot(s.t, s.Jdispr / 1000); % Ampere
 %     plot(s.t(2:end), subtracting_n_t);
-    plot(s.t(2:end), subtracting_n_intr_t * 1e3); % mA
-    plot(s.t(2:end), subtracting_n_contacts_t * 1e3); % mA
-    plot(s.t(2:end), subtracting_a_t * 1e3); % mA
-    plot(fit_t, s.params.Vapp_func(fit_coeff, fit_t))
-    legend_array = ["Total J", "Displacement J", "Charges intrinsic", "Charges contacts", "Displaced ions", "Fit"];
-    if isfield(s, 'Jidiff_points')
+    plot(s.t(2:end), subtracting_n_intr_t);
+    plot(s.t(2:end), subtracting_n_contacts_t);
+    plot(s.t(2:end), subtracting_a_t);
+    plot(fit_t, s.params.J_func(fit_coeff, fit_t), 'g')
+    plot(fit_t, J_outphase, 'r')
+    legend_array = ["Total J", "Displacement J", "Charges intrinsic", "Charges contacts", "Displaced ions", "Fit", "Out of Phase Current"];
+    if isfield(s.params, 'Jpoints')
 %         plot(s.t, s.Jidiff_points(:,end), 'k');
-        plot(s.t, s.Jidrift_points(:,end), 'k.-');
-        legend_array = [legend_array, "Ionic drift"];
+        plot(s.t, s.Jidrift_points(:,end) / 1000, 'k.-'); % in Ampere
+        plot(fit_t, s.params.J_func(fit_idrift_coeff, fit_t), 'g--');
+        legend_array = [legend_array, "Ionic drift", "Ion drift fit"];
     end
-    ylabel('Current [mA/cm2]');
+    ylabel('Current [A/cm2]');
     hold off
     xlabel('Time [s]');
     legend(legend_array);
