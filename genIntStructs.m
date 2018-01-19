147df2a4238e8b65fcf70ee0d0156ffe025db873
Ilario Gelmetti <iochesonome@gmail.com>
2018-01-19 17:14:39 +0100

    genIntStructs benjaminization and a fix

--- genIntStructs.m
+++ genIntStructs.m
@@ -1,6 +1,38 @@
 function structCell = genIntStructs(struct_eq, struct_light, startInt, endInt, points)
-% structCell generates a cell containing structures of solutions at various light
-% intensities
+%GENINTSTRUCTS - Generates a cell containing structures of solutions at various light intensities
+%
+% Syntax:  structCell = genIntStructs(struct_eq, struct_light, startInt, endInt, points)
+%
+% Inputs:
+%   STRUCT_EQ - a solution struct as created by PINDRIFT in dark conditions, or a logic false for not having the dark.
+%   STRUCT_LIGHT - a solution struct as created by PINDRIFT with illumination.
+%   STARTINT - higher requested illumination.
+%   ENDINT - lower requested illumination.
+%   POINTS - number of illumination requested between STARTINT and ENDINT, including extrema, except dark.
+%
+% Outputs:
+%   STRUCTCELL - a cell containing structs of solutions at various light
+%     intensities
+%
+% Example:
+%   genIntStructs(ssol_i_eq, ssol_i_light, 100, 0.1, 4)
+%     prepare solutions at 100, 10, 1, 0.1 and 0 illumination intensities
+%   genIntStructs(false, ssol_i_light, 100, 0.1, 4)
+%     as above but without the solution at 0 illumination (dark)
+%
+% Other m-files required: changeLight
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also changeLight, pindrift.
+
+% Author: Ilario Gelmetti, Ph.D. student, perovskite photovoltaics
+% Institute of Chemical Research of Catalonia (ICIQ)
+% Research Group Prof. Emilio Palomares
+% email address: iochesonome@gmail.com  
+% October 2017; Last revision: January 2018
+
+%------------- BEGIN CODE --------------
 
 if endInt > startInt
     warning('endInt should be smaller than startInt')
@@ -10,7 +42,7 @@ struct_Int = struct_light;
 
 % define light intensity values, always decreasing
 Int_array = logspace(log10(startInt), log10(endInt), points);
-unique(Int_array, 'stable'); % remove duplicates (just in case of startInt equal to endInt) and do not change ordering
+Int_array = unique(Int_array, 'stable'); % remove duplicates (just in case of startInt equal to endInt) and do not change ordering
 
 % if stabilized dark solution is provided in input, calculate the point at dark
 % conditions using that solution
@@ -27,6 +59,7 @@ existingVars = evalin('base', 'who');
 changeLight_tmax = false; % automatic tmax for the first run
 
 for i = 1:length(Int_array)
+    disp([mfilename ' - illumination intensity ' num2str(Int_array(i))])
     name = matlab.lang.makeValidName([inputname(2) '_Int_' num2str(Int_array(i))]);
     if any(strcmp(existingVars, name)) % check if a structure with the same name already exists
         struct_Int = evalin('base', name);
@@ -42,3 +75,5 @@ for i = 1:length(Int_array)
     structCell{2, i} = name;
     assignin('base', name, struct_Int);
 end
+
+%------------- END OF CODE --------------
