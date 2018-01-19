e0210231006d92666835a92cfbb34d2e4b349f6e
Ilario Gelmetti <iochesonome@gmail.com>
2018-01-19 17:14:27 +0100

    changeLight benjaminization

--- changeLight.m
+++ changeLight.m
@@ -1,17 +1,49 @@
- function sol_Int = changeLight(struct, newInt, tmax)
-% stabilize at a new light intensity
+function struct_Int = changeLight(struct, newInt, tmax)
+%CHANGELIGHT - Stabilize solutions at a new light intensity
+%
+% Syntax:  struct_Int = changeLight(struct, newInt, tmax)
+%
+% Inputs:
+%   STRUCT - a solution struct as created by PINDRIFT.
+%   NEWINT - the requested light intensity
+%   TMAX - the initial stabilization time, can be zero for an automatic
+%     guess
+%
+% Outputs:
+%   STRUCT_INT - a solution struct at NEWINT light intensity
+%
+% Example:
+%   changeLight(ssol_i_light, 1e-3, 5)
+%     take the solution ssol_i_light and stabilize to a new light intensity
+%     of 0.001, use as stabilization time 5 seconds
+%   changeLight(ssol_i_light, 1e-3, 0)
+%     as above, but estimate a good time for stabilization
+%
+% Other m-files required: pindrift
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also genIntStructs, pindrift.
+
+% Author: Ilario Gelmetti, Ph.D. student, perovskite photovoltaics
+% Institute of Chemical Research of Catalonia (ICIQ)
+% Research Group Prof. Emilio Palomares
+% email address: iochesonome@gmail.com  
+% October 2017; Last revision: January 2018
+
+%------------- BEGIN CODE --------------
 
 p = struct.params;
 p.pulseon = 0;
 p.tmesh_type = 2;
 p.t0 = 1e-10;
 p.tpoints = 30;
-sol_Int = struct;
+struct_Int = struct;
 
 % set an initial time for stabilization tmax
 if tmax
     p.tmax = tmax;
-else
+else % if tmax was zero, estimate a good one
     if p.mui
         p.tmax = 2^(-log10(p.mui)) / 10 + 2^(-log10(p.mue_i));
     else
@@ -20,13 +52,13 @@ else
 end
 
 % warnings about stability are not needed in the first steps
-warning('off','pindrift:verifyStabilization'); 
+warning('off', 'pindrift:verifyStabilization'); 
 
 % change light intensity in small steps
-steps = 1 + ceil(abs(log10(newInt/p.Int)));
+steps = 1 + ceil(abs(log10(newInt / p.Int)));
 
 % if the step is just one, then newInt is the output of logspace function
-Int_array = logspace(log10(p.Int), log10(newInt), steps); 
+Int_array = logspace(log10(p.Int), log10(newInt), steps);
 
 % change light in steps, not needed to reach a good stabilized solution in
 % each step, so stabilization is not verified here
@@ -34,18 +66,20 @@ Int_array = logspace(log10(p.Int), log10(newInt), steps);
 for i = 2:length(Int_array)
     disp([mfilename ' - Go from light intensity ' num2str(p.Int) ' to ' num2str(Int_array(i)) ' over ' num2str(p.tmax) ' s'])
     p.Int = Int_array(i); % set new light intensity
-    sol_Int = pindrift(sol_Int, p);
+    struct_Int = pindrift(struct_Int, p);
 end
 
-while ~verifyStabilization(sol_Int.sol, sol_Int.t, 1e-8) % check stability in a strict way
+while ~verifyStabilization(struct_Int.sol, struct_Int.t, 1e-8) % check stability in a strict way
     disp([mfilename ' - Stabilizing over ' num2str(p.tmax) ' s']);
-    sol_Int = pindrift(sol_Int, p);
+    struct_Int = pindrift(struct_Int, p);
     p.tmax = p.tmax * 10; % this change gets saved in sol_Int only if another cycle is performed
 end
 
 % warnings about stability can be useful again
-warning('on','pindrift:verifyStabilization');
+warning('on', 'pindrift:verifyStabilization');
 
 % just repeat the last one, for sake of paranoia yeee
 disp([mfilename ' - Stabilizing over ' num2str(p.tmax) ' s']);
-sol_Int = pindrift(sol_Int, p);
+struct_Int = pindrift(struct_Int, p);
+
+%------------- END OF CODE --------------
