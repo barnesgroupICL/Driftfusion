1c26a99c0a842a3bc61bd4c4c03be564f8772f74
Ilario Gelmetti <iochesonome@gmail.com>
2018-01-19 15:38:48 +0100

    equilibrate benjaminization

--- equilibrate.m
+++ equilibrate.m
@@ -1,4 +1,47 @@
 function [sol_eq, sol_light, ssol_eq, ssol_light, sol_i_eq, sol_i_light, ssol_i_eq, ssol_i_light] = equilibrate(name)
+%EQUILIBRATE Generate accurate initial conditions at equilibrium
+%Takes the parameters from pinParams.m file and tries
+%to obtain an equilibrium solution (as if the device has been left for
+%a long period of time). This solution can then be used as accurate
+%initial conditions for other simulations, e.g. a JV scan.
+%
+% Syntax:  [sol_eq, sol_light, ssol_eq, ssol_light, sol_i_eq, sol_i_light, ssol_i_eq, ssol_i_light] = EQUILIBRATE(NAME)
+%
+% Inputs:
+%   NAME - if is provided, saves the solution as [NAME *sol*] in the
+%   volatile base workspace.
+%
+% Outputs:
+%   sol_eq - short circuit in dark without mobile ionic defects
+%   sol_light - short circuit at 1 sun conditions without mobile ionic defects
+%   ssol_eq - open circuit in dark without mobile ionic defects
+%   ssol_light - open circuit at 1 sun conditions without mobile ionic defects
+%   sol_i_eq - short circuit in dark with mobile ionic defects
+%   sol_i_light - short circuit at 1 sun conditions with mobile ionic defects
+%   ssol_i_eq - open circuit in dark without mobile ionic defects
+%   ssol_i_light - open circuit at 1 sun conditions without mobile ionic defects
+%
+% Example:
+%   Equilibrate()
+%     generate stabilized solutions and save them to base workspace
+%   Equilibrate('new_')
+%     as above but prepending a string to the saved struct name
+%
+% Other m-files required: pindrift, paramsStruct, mobsetfun
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also pindrift, paramsStruct.
+
+% Author: Phil Calado, Ph.D., Ilario Gelmetti, Ph.D. student
+% Imperial College London
+% Research Group Prof. Jenny Nelson
+% Institute of Chemical Research of Catalonia (ICIQ)
+% Research Group Prof. Emilio Palomares
+% email address: p.calado13@imperial.ac.uk, iochesonome@gmail.com  
+% 2015; Last revision: January 2018
+
+%------------- BEGIN CODE --------------
 
 if ~nargin
     name = '';
@@ -20,8 +63,7 @@ p.pulseon = 0; % no transient illumination pulse
 p.OC = 0; % short circuit
 p.calcJ = 0; % no need for current calculation
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SC condition without ions in dark
+%% SC condition without ions in dark
 
 % freeze electrons and holes and ions for an initial solution
 p = mobsetfun(0, 0, p);
@@ -45,8 +87,7 @@ p.t0 = p.tmax/1e9;
 sol_eq = pindrift(sol_eq, p);
 assignin('base', [name 'sol_eq'], sol_eq);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SC condition without ions at 1 sun conditions
+%% SC condition without ions at 1 sun conditions
 
 p = sol_eq.params;
 p.Int = 1;
@@ -54,8 +95,7 @@ p.Int = 1;
 sol_light = pindrift(sol_eq, p);
 assignin('base', [name 'sol_light'], sol_light);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% OC condition without ions at equilibrium
+%% OC condition without ions at equilibrium
 
 symsol = symmetricize(sol_eq);
 
@@ -65,8 +105,7 @@ p.OC = 1;
 ssol_eq = pindrift(symsol, p);
 assignin('base', [name 'ssol_eq'], ssol_eq);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% OC condition without ions at 1 sun conditions
+%% OC condition without ions at 1 sun conditions
 
 p = ssol_eq.params;
 p.Int = 1;
@@ -74,8 +113,7 @@ p.Int = 1;
 ssol_light = pindrift(ssol_eq, p);
 assignin('base', [name 'ssol_light'], ssol_light);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SC condition with ions in dark
+%% SC condition with ions in dark
 
 p = sol_eq.params;
 
@@ -85,8 +123,7 @@ p.mui = original_p.mui; % ionic mobility
 sol_i_eq = pindrift(sol_eq, p);
 assignin('base', [name 'sol_i_eq'], sol_i_eq);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SC condition with ions at 1 sun conditions
+%% SC condition with ions at 1 sun conditions
 
 p = sol_i_eq.params;
 p.Int = 1;
@@ -94,8 +131,7 @@ p.Int = 1;
 sol_i_light = pindrift(sol_i_eq, p);
 assignin('base', [name 'sol_i_light'], sol_i_light);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% OC condition with ions in dark
+%% OC condition with ions in dark
 
 symsol = symmetricize(sol_i_eq);
 
@@ -115,8 +151,7 @@ warning('on', 'pindrift:verifyStabilization'); % reenable the warnings
 ssol_i_eq = pindrift(ssol_i_eq, p);
 assignin('base', [name 'ssol_i_eq'], ssol_i_eq);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% OC condition with ions at 1 sun conditions
+%% OC condition with ions at 1 sun conditions
 
 p = ssol_i_eq.params;
 p.Int = 1;
@@ -136,3 +171,5 @@ assignin('base', [name 'ssol_i_light'], ssol_i_light);
 evalin('base', 'clear symsol')
 evalin('base', 'clear sol')
 evalin('base', 'clear ssol')
+
+%------------- END OF CODE --------------
