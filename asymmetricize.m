3553975e4c93f2e2976d1020847f3c8ae674fc97
Ilario Gelmetti <iochesonome@gmail.com>
2018-01-22 13:33:45 +0100

    asymmetricize benjaminized

--- asymmetricize.m
+++ asymmetricize.m
@@ -1,18 +1,42 @@
-function [sol, newVoc] = asymmetricize(ssol, BC)
-% break the symmetrical solution in two halves and stabilize the
-% unsymmetrical model at pseudo-OC conditions applying Vapp equal to Voc
-% taken from the symmetrical one
+function [asymstruct, newVoc] = asymmetricize(symstruct, BC)
+%ASYMMETRICIZE - Break a symmetrical solution in two halves and stabilize the
+% asymmetrical model at pseudo-OC conditions applying Vapp equal to Voc
+% taken from the symmetrical solution
+%
+% Syntax:  [sol, newVoc] = asymmetricize(ssol, BC)
+%
+% Inputs:
+%   SYMSTRUCT - a symmetric struct as created by pindrift using the OC
+%     parameter set to 1
+%
+% Outputs:
+%
+%
+% Example:
+%   [sol_i_light_OC, newVoc] = asymmetricize(ssol_i_light, 1)
+%     take the first half of symmetrical solution at open circuit
+%
+% Other m-files required: pindrift
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also pindrift.
 
-p = ssol.params;
+% Author: Ilario Gelmetti, Ph.D. student, perovskite photovoltaics
+% Institute of Chemical Research of Catalonia (ICIQ)
+% Research Group Prof. Emilio Palomares
+% email address: iochesonome@gmail.com  
+% October 2017; Last revision: November 2017
+
+%------------- BEGIN CODE --------------
+
+p = symstruct.params;
 p.OC = 0; % without setting OC to 0 BC gets ignored, OC 0 is needed for the asymmetric solution
-p.BC = BC; % 
+p.BC = BC;
 
 % get the Voc value in the middle of the symmetrical solution at the final time step
-Voc = ssol.Voc(end);
+Voc = symstruct.Voc(end);
 p.Vapp = Voc; % use potential value in the middle as new applied voltage, this is needed for BC 0
-disp([mfilename ' - Symmetric solution voltage was ' num2str(Voc)])
-
-% p.calcJ = 2; % verify that we're at open circuit equivalent conditions, looking at the current
 
 % set an initial time for stabilization tmax
 if p.mui
@@ -22,11 +46,13 @@ else
 end
 
 % halve the solution
-centerIndex = ceil(length(ssol.x) / 2); % get the index of the middle of the x mesh
-sol_ic.sol = ssol.sol(end, 1:centerIndex, :); % take the first spatial half of the matrix, at last time step
-sol_ic.x = ssol.x(1:centerIndex); % providing just sol to pindrift is not enough, a more complete structure is needed, including at least x mesh
+centerIndex = ceil(length(symstruct.x) / 2); % get the index of the middle of the x mesh
+sol_ic.sol = symstruct.sol(end, 1:centerIndex, :); % take the first spatial half of the matrix, at last time step
+sol_ic.x = symstruct.x(1:centerIndex); % providing just sol to pindrift is not enough, a more complete structure is needed, including at least x mesh
 
 % stabilize the divided solution
-sol = pindrift(sol_ic, p); % should already be stable...
-newVoc = sol.Efn(end) - sol.Efp(1);
-disp([mfilename ' - Asymmetrized solution voltage is ' num2str(newVoc)])
+asymstruct = pindrift(sol_ic, p); % should already be stable...
+newVoc = asymstruct.Efn(end) - asymstruct.Efp(1);
+disp([mfilename ' - Symmetric solution voltage was ' num2str(Voc) '; asymmetrized solution voltage is ' num2str(newVoc)])
+
+%------------- END OF CODE --------------
