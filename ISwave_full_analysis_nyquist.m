7ac45592b166fba5c216e80a56e21ab66b832c6d
Ilario Gelmetti <iochesonome@gmail.com>
2018-01-23 15:20:19 +0100

    ISwave_full_analysis_nyquist benjaminized

--- ISwave_full_analysis_nyquist.m
+++ ISwave_full_analysis_nyquist.m
@@ -1,6 +1,30 @@
 function ISwave_full_analysis_nyquist(ISwave_struct)
-% plot the Nyquist graph for Impedance Spectroscopy (IS) in a range of background light intensities,
-% capacitance versus voltage oscillation frequency at various light bias
+%ISSTEP_FULL_ANALYSIS_NYQUIST - Plot Nyquist graph for Impedance Spectroscopy (ISwave) in a range
+% of background light intensities, imaginary part of capacitance versus
+% real part of capacitance
+%
+% Syntax:  ISwave_full_analysis_nyquist(ISwave_struct)
+%
+% Inputs:
+%   ISWAVE_STRUCT - a struct containing the most important results of the ISwave simulation
+%
+% Example:
+%   ISwave_full_analysis_nyquist(ISwave_struct)
+%     do plot
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also ISwave_full_exec.
+
+% Author: Ilario Gelmetti, Ph.D. student, perovskite photovoltaics
+% Institute of Chemical Research of Catalonia (ICIQ)
+% Research Group Prof. Emilio Palomares
+% email address: iochesonome@gmail.com  
+% October 2017; Last revision: January 2018
+
+%------------- BEGIN CODE --------------
 
 Int_colors = colormap(jet(length(ISwave_struct.Int)));
 legend_Int = string(ISwave_struct.Int);
@@ -15,3 +39,4 @@ figure('Name', 'Nyquist plot of IS at various light intensities', 'NumberTitle',
     ylabel('Im(Z) [\Omega cm^2]');
     legend(legend_Int)
     
+%------------- END OF CODE --------------
