49cae5d68a0f3da46fd3f5c023d17baf4c37e861
Ilario Gelmetti <iochesonome@gmail.com>
2018-01-23 19:02:34 +0100

    ISwave_full_exec use genIntStructs

--- ISwave_full_exec.m
+++ ISwave_full_exec.m
@@ -1,15 +1,16 @@
-function ISwave_struct = ISwave_full_exec(symstruct_eq, symstruct_light, startInt, endInt, Int_points, startFreq, endFreq, Freq_points, deltaV, BC, frozen_ions, calcJi, parallelize, save_solutions, save_result)
+function ISwave_struct = ISwave_full_exec(symstructs, startFreq, endFreq, Freq_points, deltaV, BC, frozen_ions, calcJi, parallelize, save_solutions, save_result)
 % do Impedance Spectroscopy (IS) in a range of background light intensities
 % applying an oscillating voltage
 
-symstruct_Int = symstruct_light;
-output_name = inputname(2);
+% in case a single struct is given in input, convert it to a cell structure
+% with just one cell
+if length(symstructs(:, 1)) == 1 % if the input is a single structure instead of a cell with structures
+    symstructs_temp = cell(2, 1);
+    symstructs_temp{1, 1} = symstructs;
+    symstructs_temp{2, 1} = inputname(1);
+    symstructs = symstructs_temp;
+end
 
-% in case ions should be frozen during IS, set the mobility to zero after stabilization at new light intensity
-original_p = symstruct_Int.params;
-
-% decrease annoiance by figures popping up
-symstruct_Int.params.figson = 0;
 % don't display figures until the end of the script, as they steal the focus
 % taken from https://stackoverflow.com/questions/8488758/inhibit-matlab-window-focus-stealing
 set(0, 'DefaultFigureVisible', 'off');
@@ -26,27 +27,13 @@ tpoints_per_period = 10 * 4; % gets redefined by changeLight, so re-setting is n
 % fitting, improving the tollerance is useful
 RelTol = 1e-4;
 
-% define light intensity values, always decreasing
-Int_array = logspace(log10(startInt), log10(endInt), Int_points);
-
-% if stabilized dark solution is provided in input, calculate the point at dark
-% conditions using that solution
-if isa(symstruct_eq, 'struct') % dark calculation can be disabled using "false" as first command argument
-    Int_array = [Int_array, 0];
-    % decrease annoiance by figures popping up
-    symstruct_eq.params.figson = 0;
-end
-% anyway dark gives strange results
-
-Int_array = unique(Int_array); % remove duplicates, useful just in case dark is selected as the only illumination condition from input options
-Int_array = sort(Int_array, 'descend'); % from high intensity to low, needed by changelight in case also dark is included, otherwise logspace from zero to something breaks
-
 % define frequency values
 Freq_array = logspace(log10(startFreq), log10(endFreq), Freq_points);
 
 % pre allocate arrays filling them with zeros
-Voc_array = zeros(length(Int_array));
-tmax_matrix = zeros(length(Int_array), length(Freq_array));
+Voc_array = zeros(length(symstructs(1, :)));
+Int_array = Voc_array;
+tmax_matrix = zeros(length(symstructs(1, :)), length(Freq_array));
 J_bias = tmax_matrix;
 J_amp = tmax_matrix;
 J_phase = tmax_matrix;
@@ -63,41 +50,33 @@ else
   parforArg = 0;
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% do a serie of IS measurements
-disp([mfilename ' - Doing the IS at various light intensities']);
-% set zero time for stabilization tmax, so changeLight will determine a good one
-changeLight_tmax = 0;
+%% do a serie of IS measurements
 
-for i = 1:length(Int_array)
+disp([mfilename ' - Doing the IS at various light intensities']);
+for i = 1:length(symstructs(1, :))
+    Int_array(i) = symstructs{1, i}.params.Int;
+    % decrease annoiance by figures popping up
+    symstructs{1, i}.params.figson = 0;
+    [asymstruct_Int, Voc_array(i)] = asymmetricize(symstructs{1, i}, BC); % normal BC 1 should work, also BC 2 can be employed
     if frozen_ions
-        symstruct_Int.params.mui = original_p.mui; % reset default value of ion mobility in case frozen_ions option was used
-    end
-    if Int_array(i) % in dark use the symstruct_eq solution, needed for no ions case where changeLight cannot reach dark
-        symstruct_Int = changeLight(symstruct_Int, Int_array(i), changeLight_tmax); % decrease light intensity, on the symmetrical solution
-        changeLight_tmax = symstruct_Int.params.tmax / 2; % time to use for next iteration
-    else
-        symstruct_Int = symstruct_eq;
-    end
-    [asymstruct_Int, Voc_array(i)] = asymmetricize(symstruct_Int, BC); % normal BC 1 should work, also BC 2 can be employed
-    if frozen_ions
-        asymstruct_Int.params.mui = 0; % if frozen_ions, freezing ions
+        asymstruct_Int.params.mui = 0; % if frozen_ions option is set, freezing ions
     end
     parfor (j = 1:length(Freq_array), parforArg)
         tempRelTol = RelTol; % convert RelTol variable to a temporary variable, as suggested for parallel loops
         asymstruct_ISwave = ISwave_single_exec(asymstruct_Int, BC, deltaV, Freq_array(j), periods, tpoints_per_period, calcJi, tempRelTol); % do IS
         if save_solutions && ~parallelize % assignin cannot be used in a parallel loop, so single solutions cannot be saved
+            sol_name = matlab.lang.makeValidName([symstructs{2, i} '_Freq_' num2str(Freq_array(j)) '_ISwave']);
             asymstruct_ISwave.params.figson = 1; % re-enable figures by default when using the saved solution, that were disabled above
-            sol_name = matlab.lang.makeValidName([output_name '_Int_' num2str(Int_array(i)) '_Freq_' num2str(Freq_array(j)) '_ISwave']);
-            asymstruct_ISwave.params.figson = 1;
             assignin('base', sol_name, asymstruct_ISwave);
         end
+        % set ISwave_single_analysis minimal_mode is true if parallelize is true
         [fit_coeff, fit_idrift_coeff, ~, ~, ~, ~, ~, ~] = ISwave_single_analysis(asymstruct_ISwave, parallelize); % extract parameters and do plot
         % if phase is small or negative, double check increasing accuracy of the solver
         if fit_coeff(3) < 0.03
             disp([mfilename ' - Fitted phase is very small or negative, double checking with higher solver accuracy'])
             tempRelTol = tempRelTol / 100;
             asymstruct_ISwave = ISwave_single_exec(asymstruct_Int, BC, deltaV, Freq_array(j), periods, tpoints_per_period, calcJi, tempRelTol); % do IS
+            % set ISwave_single_analysis minimal_mode is true if parallelize is true
             [fit_coeff, fit_idrift_coeff, ~, ~, ~, ~, ~, ~] = ISwave_single_analysis(asymstruct_ISwave, parallelize); % repeat analysis on new solution
         end
         % if the phase is negative even with the new accuracy, check again
@@ -105,6 +84,7 @@ for i = 1:length(Int_array)
             disp([mfilename ' - Fitted phase is extremely small, increasing solver accuracy again'])
             tempRelTol = tempRelTol / 100;
             asymstruct_ISwave = ISwave_single_exec(asymstruct_Int, BC, deltaV, Freq_array(j), periods, tpoints_per_period, calcJi, tempRelTol); % do IS
+            % set ISwave_single_analysis minimal_mode is true if parallelize is true
             [fit_coeff, fit_idrift_coeff, ~, ~, ~, ~, ~, ~] = ISwave_single_analysis(asymstruct_ISwave, parallelize); % repeat analysis on new solution
         end
         J_bias(i, j) = fit_coeff(1); % not really that useful
@@ -125,7 +105,7 @@ sun_index = find(Int_array == 1); % could used for plotting... maybe...
 % even if here the frequency is always the same for each illumination, it
 % is not the case for ISstep, and the solution has to be more similar in
 % order to be used by the same IS_full_analysis_vsfrequency script
-Freq_matrix = repmat(Freq_array, length(Int_array), 1);
+Freq_matrix = repmat(Freq_array, length(symstructs(1, :)), 1);
 
 % deltaV is a scalar, J_amp and J_phase are matrices
 % as the current of MPP is defined as positive in the model, we expect that
@@ -138,7 +118,7 @@ impedance_abs = -deltaV ./ J_amp; % J_amp is in amperes
 % current-voltage "delay"
 impedance_re = impedance_abs .* cos(J_phase); % this is the resistance
 impedance_im = impedance_abs .* sin(J_phase);
-pulsatance_matrix = 2 * pi * repmat(Freq_array, length(Int_array), 1);
+pulsatance_matrix = 2 * pi * repmat(Freq_array, length(symstructs(1, :)), 1);
 % the capacitance is the imaginary part of 1/(pulsatance*complex_impedance)
 % or can be obtained in the same way with Joutphase/(pulsatance*deltaV)
 cap = sin(J_phase) ./ (pulsatance_matrix .* impedance_abs);
@@ -150,7 +130,7 @@ cap_idrift = sin(J_i_phase) ./ (pulsatance_matrix .* impedance_i_abs);
 
 % save results, this struct is similar to ISstep_struct in terms of fields,
 % but the columns and rows in the fields can be different
-ISwave_struct.sol_name = inputname(2);
+ISwave_struct.sol_name = symstructs{2, 1};
 ISwave_struct.Voc = Voc_array;
 ISwave_struct.periods = periods;
 ISwave_struct.Freq = Freq_matrix;
@@ -175,10 +155,9 @@ ISwave_struct.impedance_i_abs = impedance_i_abs;
 ISwave_struct.impedance_i_im = impedance_i_im;
 ISwave_struct.impedance_i_re = impedance_i_re;
 if save_result
-    assignin('base', ['ISwave_' output_name], ISwave_struct);
+    assignin('base', ['ISwave_' symstructs{2, 1}], ISwave_struct);
 end
 
-% IS_full_analysis_vsvoltage(ISwave_struct); % there's no such script yet
 IS_full_analysis_vsfrequency(ISwave_struct);
 ISwave_full_analysis_nyquist(ISwave_struct);
 
