eae88a4fef79f2422ea07f37a1f0d7d4afa747e3
Ilario Gelmetti <iochesonome@gmail.com>
2018-01-29 17:01:31 +0100

    ISwave_full_analysis_nyquist improve graphics

--- ISwave_full_analysis_nyquist.m
+++ ISwave_full_analysis_nyquist.m
@@ -28,17 +28,39 @@ function ISwave_full_analysis_nyquist(ISwave_struct)
 
 %------------- BEGIN CODE --------------
 
-Int_colors = colormap(jet(length(ISwave_struct.Int)));
-legend_Int = string(ISwave_struct.Int);
+% increase graphics font size
+set(0, 'defaultAxesFontSize', 30)
+
+% create a color array with one color more than necessary
+jet_matrix = jet(length(ISwave_struct.Int) + 1);
+% find the yellow (which in RGB code is 1,1,0) and remove it from the
+% colors list
+jet_yellow_logical = ismember(jet_matrix, [1, 1, 0], 'rows');
+jet_no_yellow = jet_matrix(~jet_yellow_logical, :);
+Int_colors = colormap(jet_no_yellow);
+
+% flip array and matrixes for starting from dark
+legend_Int = string(flipud(ISwave_struct.Int));
+re_flip = flipud(ISwave_struct.impedance_re);
+im_flip = flipud(ISwave_struct.impedance_im);
+
+% add sun to numbers in legend
+legend_Int = strcat(legend_Int, ' sun');
+
+% replace zero in legend with dark
+legend_Int(legend_Int=="0 sun") = "dark";
 
 figure('Name', 'Nyquist plot of IS at various light intensities', 'NumberTitle', 'off')
     hold off
     for i = 1:length(ISwave_struct.Int)
-        plot(ISwave_struct.impedance_re(i, :), ISwave_struct.impedance_im(i, :)', 'Color', Int_colors(i, :), 'Marker', 'd');
+        plot(re_flip(i, :), im_flip(i, :)',...
+            'Color', Int_colors(i, :), 'MarkerEdgeColor', Int_colors(i, :),...
+            'MarkerFaceColor', Int_colors(i, :), 'Marker', 's',...
+            'MarkerSize', 3, 'LineWidth', 1.3);
         hold on
     end
     xlabel('Re(Z) [\Omega cm^2]');
-    ylabel('Im(Z) [\Omega cm^2]');
+    ylabel('-Im(Z) [\Omega cm^2]');
     legend(legend_Int)
     
 %------------- END OF CODE --------------
