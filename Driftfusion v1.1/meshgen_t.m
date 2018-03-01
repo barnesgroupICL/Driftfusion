b6dc5c4eaa8e1048e7166229b78dcf0940104fca
Ilario Gelmetti <iochesonome@gmail.com>
2018-03-01 15:28:24 +0100

    meshgen_t move from if to switch

--- Driftfusion v1.1/meshgen_t.m
+++ Driftfusion v1.1/meshgen_t.m
@@ -3,25 +3,22 @@ function [t] = meshgen_t(params)
 v2struct(params);
 
 % define solution mesh either logarithmically or linearly spaced points
-if tmesh_type == 1
+switch p.tmesh_type
+    case 1
+        t = linspace(0,tmax,tpoints);
 
-    t = linspace(0,tmax,tpoints);
-    %xspace = linspace(0,xmax,pp+pii+pn);      % Array of point values for optical interp1
-
-elseif tmesh_type == 2
-   
-    t = logspace(log10(t0),log10(tmax),tpoints) - t0;
+    case 2
+        t = logspace(log10(t0),log10(tmax),tpoints) - t0;
 
     % For use with TPV only! Start with 100 points up to the decay then
     % switches to log
-elseif tmesh_type == 3
-    
-    t = [linspace(0, pulsestart+pulselen, 0.2*tpoints), pulsestart + pulselen + logspace(log10(pulselen/(0.8*tpoints*1e3)), log10(tmax- pulsestart- pulselen), 0.8*tpoints)];
-   
-elseif tmesh_type == 4
-    
-    t = [linspace(0, pulsestart, 0.1*tpoints), pulsestart+ logspace(log10((pulselen/(0.7*tpoints*1e3))), log10(pulselen), 0.2*tpoints), pulsestart + pulselen + logspace(log10(pulselen/(0.7*tpoints*1e3)), log10(tmax- pulsestart- pulselen), 0.8*tpoints)];
-    
+    case 3
+        t = [linspace(0, pulsestart+pulselen, 0.2*tpoints), pulsestart + pulselen + logspace(log10(pulselen/(0.8*tpoints*1e3)), log10(tmax- pulsestart- pulselen), 0.8*tpoints)];
+
+    case 4
+        t = [linspace(0, pulsestart, 0.1*tpoints), pulsestart+ logspace(log10((pulselen/(0.7*tpoints*1e3))), log10(pulselen), 0.2*tpoints), pulsestart + pulselen + logspace(log10(pulselen/(0.7*tpoints*1e3)), log10(tmax- pulsestart- pulselen), 0.8*tpoints)];
+    otherwise
+        error('DrIFtFUSION:tmesh_type', [mfilename ' - tmesh_type not recognized'])
 end
 
 end
