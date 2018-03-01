202beb185764b53dd113740c238dbf8a6cf74028
Ilario Gelmetti <iochesonome@gmail.com>
2018-03-01 15:28:59 +0100

    meshgen_t get rid of v2struct

--- Driftfusion v1.1/meshgen_t.m
+++ Driftfusion v1.1/meshgen_t.m
@@ -1,22 +1,23 @@
-function [t] = meshgen_t(params)
-
-v2struct(params);
+function [t] = meshgen_t(p)
 
 % define solution mesh either logarithmically or linearly spaced points
 switch p.tmesh_type
     case 1
-        t = linspace(0,tmax,tpoints);
+        t = linspace(0, p.tmax, p.tpoints);
 
     case 2
-        t = logspace(log10(t0),log10(tmax),tpoints) - t0;
+        t = logspace(log10(p.t0), log10(p.tmax), p.tpoints) - p.t0;
 
     % For use with TPV only! Start with 100 points up to the decay then
     % switches to log
     case 3
-        t = [linspace(0, pulsestart+pulselen, 0.2*tpoints), pulsestart + pulselen + logspace(log10(pulselen/(0.8*tpoints*1e3)), log10(tmax- pulsestart- pulselen), 0.8*tpoints)];
+        t = [linspace(0, p.pulsestart+p.pulselen, 0.2*p.tpoints),...
+            p.pulsestart + p.pulselen + logspace(log10(p.pulselen/(0.8*p.tpoints*1e3)), log10(p.tmax-p.pulsestart-p.pulselen), 0.8*p.tpoints)];
 
     case 4
-        t = [linspace(0, pulsestart, 0.1*tpoints), pulsestart+ logspace(log10((pulselen/(0.7*tpoints*1e3))), log10(pulselen), 0.2*tpoints), pulsestart + pulselen + logspace(log10(pulselen/(0.7*tpoints*1e3)), log10(tmax- pulsestart- pulselen), 0.8*tpoints)];
+        t = [linspace(0, p.pulsestart, 0.1*p.tpoints),...
+            p.pulsestart + logspace(log10(p.pulselen/(0.7*p.tpoints*1e3)), log10(p.pulselen), 0.2*p.tpoints),...
+            p.pulsestart + p.pulselen + logspace(log10(p.pulselen/(0.7*p.tpoints*1e3)), log10(p.tmax-p.pulsestart-p.pulselen), 0.8*p.tpoints)];
     otherwise
         error('DrIFtFUSION:tmesh_type', [mfilename ' - tmesh_type not recognized'])
 end
