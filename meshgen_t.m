4ddd2c669840ddc143d04445879628ab148c26dc
Ilario Gelmetti <iochesonome@gmail.com>
2018-03-01 15:46:25 +0100

    meshgen_* reflect changes proposed upstream

--- meshgen_t.m
+++ meshgen_t.m
@@ -1,38 +1,25 @@
 function [t] = meshgen_t(p)
 
-% v2struct(params);
-
-% change for plotting time mesh
-meshfigon = false;
-
 % define solution mesh either logarithmically or linearly spaced points
-switch p.tmesh_type 
+switch p.tmesh_type
     case 1
-        t = linspace(0,p.tmax,p.tpoints);
-        %xspace = linspace(0,xmax,pp+pii+pn);      % Array of point values for optical interp1
+        t = linspace(0, p.tmax, p.tpoints);
 
     case 2
-        t = logspace(log10(p.t0),log10(p.tmax),p.tpoints) - p.t0;
-        % For use with TPV only! Start with 100 points up to the decay then
-        % switches to log
-        
+        t = logspace(log10(p.t0), log10(p.tmax), p.tpoints) - p.t0;
+
+    % For use with TPV only! Start with 100 points up to the decay then
+    % switches to log
     case 3
-        t = [linspace(0, p.pulsestart + p.pulselen, 0.2 * p.tpoints), p.pulsestart + p.pulselen + logspace(log10(p.pulselen/(0.8*p.tpoints*1e3)), log10(p.tmax - p.pulsestart- p.pulselen), 0.8*p.tpoints)];
-        
+        t = [linspace(0, p.pulsestart+p.pulselen, 0.2*p.tpoints),...
+            p.pulsestart + p.pulselen + logspace(log10(p.pulselen/(0.8*p.tpoints*1e3)), log10(p.tmax-p.pulsestart-p.pulselen), 0.8*p.tpoints)];
+
     case 4
-        t = [linspace(0, p.pulsestart, 0.1*p.tpoints), p.pulsestart+ logspace(log10((p.pulselen/(0.7*p.tpoints*1e3))), log10(p.pulselen), 0.2*p.tpoints), p.pulsestart + p.pulselen + logspace(log10(p.pulselen/(0.7*p.tpoints*1e3)), log10(p.tmax- p.pulsestart- p.pulselen), 0.8*p.tpoints)];
-    otherwise % will never happen
-        t = 0;
-        error('tmesh_type not recognized')
+        t = [linspace(0, p.pulsestart, 0.1*p.tpoints),...
+            p.pulsestart + logspace(log10(p.pulselen/(0.7*p.tpoints*1e3)), log10(p.pulselen), 0.2*p.tpoints),...
+            p.pulsestart + p.pulselen + logspace(log10(p.pulselen/(0.7*p.tpoints*1e3)), log10(p.tmax-p.pulsestart-p.pulselen), 0.8*p.tpoints)];
+    otherwise
+        error('DrIFtFUSION:tmesh_type', [mfilename ' - tmesh_type not recognized'])
 end
-
-if meshfigon
-
-    ptmir = 1:1:length(t);
-    
-    figure(200);
-    plot(t, ptmir, '.');
-    xlabel('Time');
-    ylabel('Point');
 
 end
