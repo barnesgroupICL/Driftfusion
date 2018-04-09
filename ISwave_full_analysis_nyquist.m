a97ac88204e7b3d494984ef016e2da02b439377c
Ilario Gelmetti <iochesonome@gmail.com>
2018-04-09 17:46:00 +0200

    ISwave_full_analysis_nyquist new plot normalized

--- ISwave_full_analysis_nyquist.m
+++ ISwave_full_analysis_nyquist.m
@@ -87,5 +87,50 @@ figure('Name', 'Nyquist plot of IS at various light intensities', 'NumberTitle',
     ylabel('-Im(Z) [\Omega cm^2]');
     legend(flipud(h(h~=0)), legend_flip)
     legend boxoff
+
+% preallocate figures handles
+h = zeros(length(legend_text), 1);
+% function for normalization fitting
+%semicircle = @(coeff, re) real((coeff(1).^2 - (re - coeff(2)).^2).^0.5);
+figure('Name', 'Normalized Nyquist plot of IS at various light intensities', 'NumberTitle', 'off')
+    hold off
+    % take the last point orizontally
+    %norm_array = ISwave_struct.impedance_re(:, end) / min(ISwave_struct.impedance_re(:, end));
+    % take the maximum point vertically
+    max_array = max(-ISwave_struct.impedance_im, [], 2);
+    norm_array = max_array / min(max_array);
+    for i = 1:length(legend_text)
+%         fit_re = ISwave_struct.impedance_re(i, end-9:end);
+%         fit_im = -ISwave_struct.impedance_im(i, end-9:end);
+%         try
+%             fit = fitnlm(fit_re, fit_im, semicircle, [max_array(i).^2, max_array(i)]);
+%             norm_array(i) = sqrt(fit.Coefficients.Estimate(1));
+%         catch
+%             disp([mfilename ' - Failed fit for ' legend_flip(i)])
+%         end
+        
+        % find the points of the decades
+        temp_logfreq = log10(ISwave_struct.Freq(i,:));
+        evendecade_index = mod(temp_logfreq, 2) == 0;
+        
+        h(i) = plot(ISwave_struct.impedance_re(i, :) / norm_array(i), -ISwave_struct.impedance_im(i, :)' / norm_array(i),...
+            'Color', Int_colors(i, :), 'MarkerEdgeColor', Int_colors(i, :),...
+            'MarkerFaceColor', Int_colors(i, :), 'Marker', 's',...
+            'MarkerSize', 3, 'LineWidth', 1.3);
+        hold on
+        
+        % add bigger markers for the points of the decades
+        plot(ISwave_struct.impedance_re(i, evendecade_index) / norm_array(i), -ISwave_struct.impedance_im(i, evendecade_index)' / norm_array(i),...
+            'MarkerFaceColor', Int_colors(i, :), 'MarkerEdgeColor', Int_colors(i, :),...
+            'Marker', 'x', 'LineStyle', 'none', 'MarkerSize', 12);
+    end
+    xlabel('Re(Z) [a.u.]');
+    ylabel('-Im(Z) [a.u.]');
+    % add the normalization to the legend
+    legend_flip_norm = strcat(legend_flip, " / ", flipud(num2str(round(norm_array, 2, 'significant'))));
+    % remove unneeded spaces from legend
+    legend_flip_norm = regexprep(legend_flip_norm, ' +', ' ');
+    legend(flipud(h(h~=0)), legend_flip_norm)
+    legend boxoff
     
 %------------- END OF CODE --------------
