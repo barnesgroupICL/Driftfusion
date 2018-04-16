3a773654786b9fce1abb01dac2fa85b525e7cbd1
Ilario Gelmetti <iochesonome@gmail.com>
2018-04-16 13:32:30 +0200

    ISwave_subtracting_analysis improve gradient calculation, result with the same dimension of time array

--- ISwave_subtracting_analysis.m
+++ ISwave_subtracting_analysis.m
@@ -43,16 +43,12 @@ itype_points= (s.x >= s.p.tp & s.x <= s.p.tp + s.p.ti);
 % profile at the previous time point, the resulting rows will be one row
 % less than the original solution matrix
 
-% for simulating a current, taking half of the abs is the right way
-% I divide by two 'cause otherwise I would calculate once the charge
-% getting to the new place and another time the charge missing in the old place
-n_delta_matrix = s.sol(2:end, :, 1) - s.sol(1:end-1, :, 1);
-i_delta_matrix = s.sol(2:end, :, 3) - s.sol(1:end-1, :, 3);
-i_delta_abs_matrix = abs(s.sol(2:end, :, 3) - s.sol(1:end-1, :, 3)); % out of the intrinsic the difference is constantly zero
-
-% t mesh should be equally spaced for IS wave experiment, anyway better to
-% use actual time points just in case this will change in the future
-t_array = s.t(2:end) - s.t(1:end-1);
+% now I'm doing the gradient and then integrating it in the other
+% dimension, it would be more efficient to do the integration before
+% differentiating
+[~, n_delta_matrix] = gradient(s.sol(:, :, 1), s.x, s.t);%s.sol(2:end, :, 1) - s.sol(1:end-1, :, 1);
+[~, i_delta_matrix] = gradient(s.sol(:, :, 3), s.x, s.t);%s.sol(2:end, :, 3) - s.sol(1:end-1, :, 3);
+i_delta_abs_matrix = abs(i_delta_matrix); % out of the intrinsic the difference is constantly zero
 
 % sum of the electrons over thickness, obtain a point for each time
 n_delta_t = transpose(trapz(s.x, n_delta_matrix, 2));
@@ -80,13 +76,13 @@ n_delta_intr_t = transpose(trapz(s.x(itype_points), n_delta_matrix(:, itype_poin
 
 %% dividing by time delta converts the output in a current
 % convert from number of electrons to charge per time point
-subtracting_n_t = -s.p.e * n_delta_t ./ t_array;
+subtracting_n_t = -s.p.e * n_delta_t;
 % convert from number of ions to charge per time point
-subtracting_i_abs_t = s.p.e * i_delta_abs_t ./ t_array;
+subtracting_i_abs_t = s.p.e * i_delta_abs_t;
 % convert from number of ions to charge per time point
-subtracting_i_t = s.p.e * i_delta_t ./ t_array;
+subtracting_i_t = s.p.e * i_delta_t;
 % convert from number of electrons in the intrinsic to charge per time point
-subtracting_n_intr_t = -s.p.e * n_delta_intr_t ./ t_array;
+subtracting_n_intr_t = -s.p.e * n_delta_intr_t;
 % this is the same as actually doing the integral in the contacts
 subtracting_n_contacts_t = subtracting_n_t - subtracting_n_intr_t;
 
