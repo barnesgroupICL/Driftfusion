9b7ee6980709f9fbc2a6d62ac7ad59efdcdac45f
Ilario Gelmetti <iochesonome@gmail.com>
2018-04-20 16:00:28 +0200

    fromISwaveResultsToTxt added more output

--- fromISwaveResultsToTxt.m
+++ fromISwaveResultsToTxt.m
@@ -31,23 +31,48 @@ headerNyquist = ['Frequency', headerReIm'];
 
 unitsCap = ['Hz', repelem("F/cm\+(2)", length(legendImpedance))];
 unitsNyquist = ['Hz', repelem("\g(W)·cm\+(2)", 2*length(legendImpedance))];
+unitsZabs = ['Hz', repelem("\g(W)·cm\+(2)", length(legendImpedance))];
+unitsPhase = ['Hz', repelem("degrees", length(legendImpedance))];
 
 %% get data
 
 % all the lines are the same for ISwave
 frequencies = ISwave_results.Freq(1, :)';
 
+% capacitance
 cap = ISwave_results.cap';
 dataCap = [frequencies, cap];
 
+% ionic capacitance
 capIonic = ISwave_results.cap_idrift';
 dataCapIonic = [frequencies, capIonic];
 
+% recombination capacitance
+capRec = ISwave_results.cap_U';
+dataCapRec = [frequencies, capRec];
+
+% accumulating current capacitance
+capAcc = ISwave_results.cap_dQ';
+dataCapAcc = [frequencies, capAcc];
+
+% nyquist
 impedance_re = ISwave_results.impedance_re';
 impedance_im = ISwave_results.impedance_im';
 impedance = impedance_im(:, [1;1]*(1:size(impedance_im, 2)));
 impedance(:, 1:2:end) = impedance_re;
 dataNyquist = [frequencies, impedance];
+
+% absolute impedance
+impedance_abs = ISwave_results.impedance_abs';
+dataZabs = [frequencies, impedance_abs];
+
+% phase
+Zphase = -ISwave_results.J_phase';
+dataZphase = [frequencies, Zphase];
+
+% ionic phase
+ZphaseIonic = -ISwave_results.J_i_phase';
+dataZphaseIonic = [frequencies, ZphaseIonic];
 
 %% join fields
 
@@ -55,13 +80,28 @@ toBeSavedCap = [headerFrequencyIntVdc; unitsCap; dataCap];
 
 toBeSavedCapIonic = [headerFrequencyIntVdc; unitsCap; dataCapIonic];
 
+toBeSavedCapRec = [headerFrequencyIntVdc; unitsCap; dataCapRec];
+
+toBeSavedCapAcc = [headerFrequencyIntVdc; unitsCap; dataCapAcc];
+
 toBeSavedNyquist = [headerNyquist; unitsNyquist; dataNyquist];
+
+toBeSavedZabs = [headerFrequencyIntVdc; unitsZabs; dataZabs];
+
+toBeSavedZphase = [headerFrequencyIntVdc; unitsPhase; dataZphase];
+
+toBeSavedZphaseIonic = [headerFrequencyIntVdc; unitsPhase; dataZphaseIonic];
 
 %% save csv
 
-fid_cap = fopen([prefix '-cap.txt'],'wt+');
-fid_capIonic = fopen([prefix '-cap_ionic.txt'],'wt+');
-fid_nyquist = fopen([prefix '-nyquist.txt'],'wt+');
+fid_cap = fopen([prefix '-cap.txt'], 'wt+');
+fid_capIonic = fopen([prefix '-cap_ionic.txt'], 'wt+');
+fid_capRec = fopen([prefix '-cap_recombination.txt'], 'wt+');
+fid_capAcc = fopen([prefix '-cap_accumulating.txt'], 'wt+');
+fid_nyquist = fopen([prefix '-nyquist.txt'], 'wt+');
+fid_Zabs = fopen([prefix '-Zabs.txt'], 'wt+');
+fid_phase = fopen([prefix '-Zphase.txt'], 'wt+');
+fid_phaseIonic = fopen([prefix '-Zphase_ionic.txt'], 'wt+');
 
 for i = 1:size(toBeSavedCap, 1)
     fprintf(fid_cap, '%s\t', toBeSavedCap(i, :));
@@ -70,10 +110,30 @@ for i = 1:size(toBeSavedCap, 1)
     fprintf(fid_capIonic, '%s\t', toBeSavedCapIonic(i, :));
     fprintf(fid_capIonic, '\n');
     
+    fprintf(fid_capRec, '%s\t', toBeSavedCapRec(i, :));
+    fprintf(fid_capRec, '\n');
+    
+    fprintf(fid_capAcc, '%s\t', toBeSavedCapAcc(i, :));
+    fprintf(fid_capAcc, '\n');
+    
     fprintf(fid_nyquist, '%s\t', toBeSavedNyquist(i, :));
     fprintf(fid_nyquist, '\n');
+    
+    fprintf(fid_Zabs, '%s\t', toBeSavedZabs(i, :));
+    fprintf(fid_Zabs, '\n');
+    
+    fprintf(fid_phase, '%s\t', toBeSavedZphase(i, :));
+    fprintf(fid_phase, '\n');
+    
+    fprintf(fid_phaseIonic, '%s\t', toBeSavedZphaseIonic(i, :));
+    fprintf(fid_phaseIonic, '\n'); 
 end
 
 fclose(fid_cap);
 fclose(fid_capIonic);
+fclose(fid_capRec);
+fclose(fid_capAcc);
 fclose(fid_nyquist);
+fclose(fid_Zabs);
+fclose(fid_phase);
+fclose(fid_phaseIonic);
