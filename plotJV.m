cee14505daa2553101285aae51dae0efc3963cd5
pdb13 <p.calado13@imperial.ac.uk>
2018-09-19 19:12:41 +0100

    got rid of x limits

--- plotJV.m
+++ plotJV.m
@@ -3,19 +3,23 @@ function plotJV(JV, option)
 % option        1 = dark only, 2 = light only, 3 = dark & light
 % JV is a structure containing dark and illuminated JVs
 
-figure(11)
-xlim([0, 1.15])
-ylim([-25, 5]);
-hold on
+
 
 if option == 1 || option == 3
     pinana(JV.dk.f);
+    figure(11)
+%xlim([-0.2, 1.15])
+ylim([-25, 5]);
+hold on
     pinana(JV.dk.r);
     
 end
 
 if option == 2 || option == 3
-    pinana(JV.ill.f);   
+    pinana(JV.ill.f);figure(11)
+%xlim([-0.2, 1.15])
+ylim([-25, 5]);
+hold on
     pinana(JV.ill.r);
 end
 
