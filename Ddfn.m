6c5b3bf948d52a36ddc36bb0f118738a132f118f
pdb13 <p.calado13@imperial.ac.uk>
2018-10-22 14:30:36 +0100

    fixes

--- Ddfn.m
+++ Ddfn.m
@@ -1,29 +1,28 @@
 function D = Ddfn(Nc, Ec, Efn, mu, T)
-% Test function for Fermi Dirac diffusion coefficient 
+% Test function for Fermi Dirac diffusion coefficient
 % Nc = conduction band density of states
 % Ec = conduction band energy
 % Ef = Fermi level
 % T = temperature
 % See Schubert 2015, pp. 130
 
-kB = 8.617330350e-5;       % Boltzmann constant [eV K^-1]    
+kB = 8.617330350e-5;       % Boltzmann constant [eV K^-1]
 kT = kB*T;
 e = 1.61917e-19;         % Elementary charge in Coulombs.
 
 for i = 1:length(Efn)
-
+    
     f = @(E) 1./(1 + exp((E-Efn(i)+Ec)/kT));    % Fermi- Dirac function
-    dfdE = @(E) ((exp((E-Efn(i)+Ec)/kT))./(kT*((exp((E-Efn(i)+Ec)/kT)+1).^2)));
-
+    dfdE = @(E) exp((E-Efn(i)+Ec)/kT)./(kT*(exp((E-Efn(i)+Ec)/kT)+1).^2);
+    
     g = @(E) (E/kT).^0.5;                        % DOS function based on 3D semiconductor
     h = @(E) g(E).*f(E);
     k = @(E) g(E).*dfdE(E);
-
-    n(i) = real(((2*Nc)/(kT*pi^0.5))*integral(f, Ec, Inf));
-    %dndE(i) = ((2*Nc)/(kT*pi^0.5))*integral(dfdE, Ec, Inf);
+    
+    n(i) = real(((2*Nc)/(kT*pi^0.5))*integral(f, 0, 16));
+    dndE(i) = ((2*Nc)/(kT*pi^0.5))*integral(dfdE, 0, 16);
+    
 end
-
-dndE = gradient(n);
 
 D = mu*(n./dndE);
 
