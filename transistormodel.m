8b7eaad821148311750b816b51a38aa568dc9d1d
pdb13 <p.calado13@imperial.ac.uk>
2018-11-23 11:00:11 +0000

    Simple code for plotting diode curves

--- transistormodel.m
+++ transistormodel.m
@@ -4,23 +4,22 @@ function transistormodel(Vn, Js1, Js2)
 e = 1.602e-19;
 kT = 0.0257;
 Jph = -20e-3;
-J1 = 8.8e5*e;
 V = 0:0.01:1.4;
-Vn = ones(1, length(V));
+Vn = Vn*ones(1, length(V));
 Vrec = V-Vn;
 Vcol = -Vn; 
 
-J1 = Js1*(exp(Vrec/kT)-1);
-J2 = Js2*(exp(Vcol/kT)-2);
+J1 = Js1*(exp(V/kT)-1);
+J2 = Js2*(exp(V/kT)-1);
 
 J = Jph + J1;
 
 figure(600)
-plot(V, J1, V, J2)
+semilogy(V, J1, V, J2)
 xlabel('V [V]')
 ylabel('J [A]')
 legend('J1', 'J2')
-ylim([-30e-3, 30e-3])
+%ylim([-30e-3, 30e-3])
 
 figure(601)
 plot(V, J)
