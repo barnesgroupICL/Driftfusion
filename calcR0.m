9e049d4e1e8ac0de9ea41713404e00cb94cd6905
pdb13 <p.calado13@imperial.ac.uk>
2018-12-04 10:29:43 +0000

    Completed upgrade to pc input

--- calcR0.m
+++ calcR0.m
@@ -1,8 +1,8 @@
-function [JV_ana, R0, k_rad, Voc, G0] = calcR0(Eg, N0, EgArr, Jsc_vs_Eg, par)
-% calculates 
+function [JV_ana, R0, k_rad, Voc, G0] = calcR0(EgArr, Jsc_vs_Eg, par)
+% calculates
 % use calcJsc first to get Jsc_vs_Eg & egArr
 %% Input arguments
-% PAR - a parameters class containing 
+% PAR - a parameters class containing
 
 set(0,'DefaultLineLinewidth',1);
 set(0,'DefaultAxesFontSize',16);
@@ -22,54 +22,58 @@ q = 1.60217662e-19;         % Elementary charge [C]
 
 % E_dis = 1.6:0.01:1600;
 % r0_dis = ((2*pi)/(h^3*c^2))*((E_dis.^2)./(exp((E_dis/(par.kB*par.T))-1)));
-% 
+%
 % R01 = trapz(E_dis, r0_dis);
-% 
-% figure(499)   
+%
+% figure(499)
 % semilogy(E_dis, r0_dis)
 % xlabel('Energy [eV]')
 % ylabel('r0(E)')
 
 for i =1:length(par.Eg)
-
-% Find maximum Jsc based on step function absorption and 100% EQE
-p = find(EgArr <= par.Eg(i));
-p = p(end);
-
-Jsc(i) = Jsc_vs_Eg(p);
-
-fun = @(E) ((2*pi)/(h^3*c^2))*((E.^2)./(exp((E/(par.kB*par.T))-1)));
-
-J0_fd(i) = integral(fun, Eg(i), Inf);    % Spectral bb flux density- factor of 2 for back reflector
-R0(i) = J0_fd./d(i);
-
-J0(i) = J0_fd*q;
-
-k_rad(i) = R0/(par.ni(i)^2);      % cm3s-1
-
-V = -0.4:0.001:1.2;
-
-J = (-Jsc + J0*exp((V/(par.kB*par.T))-1));  %mA cm-2
-
-J_dk = (J0*exp((V/(par.kB*par.T))-1));
-
-Voc = (par.kB*par.T)*(log(Jsc/J0)+1);
-
-G0 = Jsc./(t*q);           % Generation rate cm-3s-1
-
-JV_ana.J = J;
-JV_ana.V = V;
-
-if figson == 1
-
-figure(500)
-plot(V, J, V, J_dk)
-xlabel('V [V]')
-ylabel('J[mAcm-2]')
-ylim([-50, 30])
-xlim([-inf, inf])
-grid off
-
+    
+    % Find maximum Jsc based on step function absorption and 100% EQE
+    p = find(EgArr <= par.Eg(i));
+    p = p(end);
+    
+    Jsc(i) = Jsc_vs_Eg(p);
+    
+    fun = @(E) ((2*pi)/(h^3*c^2))*((E.^2)./(exp((E/(par.kB*par.T))-1)));
+    
+    J0_fd(i) = integral(fun, par.Eg(i), Inf);    % Spectral bb flux density- factor of 2 for back reflector
+    R0(i) = J0_fd(i)./par.d(i);
+    
+    J0(i) = J0_fd(i)*q;
+    
+    k_rad(i) = R0(i)/(par.ni(i)^2);      % cm3s-1
+    
+    V = -0.4:1e-2:3.0;
+    
+    J(i,:) = -Jsc(i) + J0(i).*(exp(V./(par.kB*par.T))-1);  %A cm-2
+    
+    J_dk(i,:) = (J0(i).*exp((V/(par.kB*par.T))-1));
+    
+    Voc(i) = (par.kB*par.T).*(log(Jsc(i)./J0(i))+1);
+    
+    G0(i) = Jsc(i)./(par.d(i).*q);           % Generation rate cm-3s-1
+    
+    JV_ana.J(i,:) = J(i,:);
+    JV_ana.J_dk(i,:) = J_dk(i,:);
+    JV_ana.V = V;
+    
+    if figson == 1
+        figure(500)
+        plot(V, J(i,:), V, J_dk(i,:))
+        xlabel('V [V]')
+        ylabel('J[mAcm-2]')
+        ylim([-50, 30])
+        xlim([0, V(end)])
+        hold on
+    end
+    
 end
+
+    hold off
+
 
 end
