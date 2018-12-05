d148416fb9763052b7869437234f55da9bc13c49
pdb13 <p.calado13@imperial.ac.uk>
2018-12-05 19:09:40 +0000

    reinstated individuals functions so that they can be accessed individually
    Need to create classes with methods to tidy this all up

--- beerlambert.m
+++ beerlambert.m
@@ -127,16 +127,3 @@ end
 
 end
 
-function AM15 = lightsource(source_type, lambda)
-% loads light sources for use with BeerLambert function
-% lambda - range of wavelengths in nanometers
-
-if source_type == 'AM15'
-    
-    % Load AM1.5
-    AM15_data=xlsread('AM15.xls');
-    AM15=1e-3*interp1(AM15_data(:,1), AM15_data(:,2), lambda, 'linear', 'extrap');
-    
-end
-
-end
--- doJV.m
+++ doJV.m
@@ -111,33 +111,3 @@ if option ==2 || option ==3
 end
 
 end
-
-function plotJV(JV, option)
-% JV - a soultion from doJV
-% OPTION - 1 = dark only, 2 = light only, 3 = dark & light
-% JV is a structure containing dark and illuminated JVs
-
-
-if option == 1 || option == 3
-    dfana(JV.dk.f);
-    figure(11)
-    %xlim([-0.2, 1.15])
-    ylim([-25e-3, 5e-3]);
-    hold on
-    dfana(JV.dk.r);
-    
-end
-
-if option == 2 || option == 3
-    dfana(JV.ill.f);
-    
-    figure(11)
-    %xlim([-0.2, 1.15])
-    ylim([-25e-3, 5e-3]);
-    hold on
-    dfana(JV.ill.r);
-end
-
-hold off
-
-end
