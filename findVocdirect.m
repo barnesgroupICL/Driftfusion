f8e78801eaaa18f4c065c2c2b36e7de2ce0d44b9
pdb13 <p.calado13@imperial.ac.uk>
2019-02-11 12:51:13 +0000

    find Voc directly using series resistance

---
+++ findVocdirect.m
@@ -0,0 +1,18 @@
+function sol_Voc = findVocdirect(sol_ini, light_intensity)
+
+par = sol_ini.par;
+
+par.Rs = 1e6;
+par.tmesh_type = 1;
+par.t0 = 0;
+par.tmax = 1e-6;
+par.tpoints = 40;
+
+sol_Rs = df(sol_ini, par);
+
+par.Int = light_intensity;
+par.tmax = 1e-3;
+
+sol_Voc = df(sol_Rs, par);
+
+end
