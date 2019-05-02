d1cc33b4e68fc5708f7ae54480413b20c2eb0f11
pdb13 <p.calado13@imperial.ac.uk>
2019-05-02 11:32:19 +0100

    Added Nion = 1e19 to script

--- Helper/explorescript.m
+++ Helper/explorescript.m
@@ -1,3 +1,14 @@
-
 par.ptpd = pc('input_files/ptpd.csv');
-ptpd_parex_dactive_light_ = explore.explore2par(par.ptpd, {'dcell(1,4)','Int'}, {[10e-7, 40e-7, 90e-7, 140e-7, 340e-7], logspace(-4,1,6)}, 400);
+ptpd_parex_dactive_light_ = explore.explore2par(par.ptpd, {'dcell(1,4)','Int'}, {[10e-7, 40e-7, 90e-7, 140e-7, 340e-7, 740e-7], logspace(-3,1,6)}, 400);
+
+par.pcbm = pc('input_files/pcbm.csv');
+pcbm_parex_dactive_light_ = explore.explore2par(par.pcbm, {'dcell(1,4)','Int'}, {[10e-7, 40e-7, 90e-7, 140e-7, 340e-7, 740e-7], logspace(-3,1,6)}, 400);
+
+par.ptpd_Na19 = pc('input_files/ptpd.csv');
+par.ptpd_Na19.Nion = 1e19;
+
+ptpd_parex_dactive_light_ = explore.explore2par(par.ptpd_Na19, {'dcell(1,4)','Int'}, {[10e-7, 40e-7, 90e-7, 140e-7, 340e-7, 740e-7], logspace(-3,1,6)}, 400);
+
+par.pcbm_Na19 = pc('input_files/pcbm.csv');
+par.pcbm_Na19.Nion = 1e19;
+pcbm_parex_dactive_light_ = explore.explore2par(par.pcbm_Na19, {'dcell(1,4)','Int'}, {[10e-7, 40e-7, 90e-7, 140e-7, 340e-7, 740e-7], logspace(-3,1,6)}, 400);
