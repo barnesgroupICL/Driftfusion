da4b1be3c2084165799d5855c1c9fd7571c4a7ea
pdb13 <p.calado13@imperial.ac.uk>
2019-05-13 18:57:31 +0100

    Script for reproducing Evidence for ion

---
+++ evidence_for_ion_script.m
@@ -0,0 +1,29 @@
+%% A script to reproduce the results from Calado et al. 2016
+
+%% Load parameters
+% set OM =0 in pc before doing this for uniform generation in the active
+% layer
+par.tc = pc('input_files/Top Cathode.csv');
+par.bc = pc('input_files/Bottom Cathode.csv');
+
+%% Current-voltage scans
+
+%% Bottom cathode device
+% Get equilibrium solution
+soleq.bc = equilibrate(par.bc);
+% Scan to -1.0 V - dark only to get starting condition
+% for the JV
+% JV = doJV(sol_ini, JVscan_rate, JVscan_pnts, Intensity, mobseti, Vstart, Vend, option)
+JV.bc_0V_to_m1V = doJV(soleq.bc.ion, 1e-6, 40, 1, 0, 0, -1, 1);
+% Scan from -1.0 V to 1.4 V dark and light
+JV.bc_m1V_to_1p2V = doJV(soleq.bc.ion, 40e-3, 200, 1, 1, -1, 1.2, 3);
+
+
+%% Top cathode device
+% Get equilibrium solution
+soleq.tc = equilibrate(par.tc);
+% Scan to -1.0 V with ion mobility off- dark only to get starting condition
+% for the JV
+%JV.tc_0V_to_m1V = doJV(soleq.tc.ion, 1e-6, 100, 1, 40, 0, -1, 1);
+% Scan from -1.0 V to 1.4 V dark and light
+JV.tc_m1V_to_1p2V = doJV(soleq.tc.ion, 40e-3, 200, 1, 1, 0, 1.2, 3);
