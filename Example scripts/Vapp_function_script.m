2deb4f2b789d0eac04d208b7b10e6a65f917465c
pdb13 <p.calado13@imperial.ac.uk>
2019-05-15 14:53:29 +0100

    Bug fix and improvements to Vapp_function

--- Example scripts/Vapp_function_script.m
+++ Example scripts/Vapp_function_script.m
@@ -2,11 +2,12 @@
 
 par.singlecar = pc('input_files/1 layer single carrier.csv');
 
-soleq = equilibrate(par.singlecar);
+%soleq = equilibrate(par.singlecar);
 
 % tmax is the period (seconds)
 tmax = 10;
-coeff = [50e-3,(2*pi)/tmax,0];
+Nperiods = 2;   % Number of periods
+coeff = [50e-3, Nperiods*(2*pi)/tmax,0];
 Vapp_func = @(coeff, t) coeff(1)*sin(coeff(2)*t + coeff(3));
 
 % Vapp_function(sol_ini, Vapp_func, tmax, tpoints, logtime)
@@ -15,4 +16,4 @@ sol_Vapp_func = Vapp_function(soleq.ion, Vapp_func, coeff, tmax, 200, 0);
 % Plot outputs
 dfplot.Vappt(sol_Vapp_func)
 % Current at mid-point
-dfplot.Jt(sol_Vapp_func, round(par.singular.pcum(end)/2))
+dfplot.Jt(sol_Vapp_func, round(par.singlecar.pcum(end)/2))
