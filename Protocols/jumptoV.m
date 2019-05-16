0680837cf018edf0fcc70224f5573b7d2c994d4b
pdb13 <p.calado13@imperial.ac.uk>
2019-05-16 11:38:46 +0100

    Improved Vjump function now uses Vapp_function instead of JV

--- Protocols/jumptoV.m
+++ Protocols/jumptoV.m
@@ -1,4 +1,4 @@
-function sol_relax = jumptoV(sol_ini, Vjump, tdwell)
+function sol_relax = jumptoV(sol_ini, Vjump, tdwell, Int)
 % A function to simulate a jump-to-Voltage measurement as used for SDP
 %% Input arugments
 % SOL_INI - input solution- could be cell at equilibrium or stablised at an
@@ -8,12 +8,18 @@ function sol_relax = jumptoV(sol_ini, Vjump, tdwell)
 
 %% OUTPUTS
 % SOL_RELAX - the output relaxation solution
-
 par = sol_ini.par;
 V0 = par.Vapp;
 V1 = Vjump;
+tjump = 1e-6;   % Jump to V in 1 us
 
-jump1 = doJV(sol_ini,1,100,0,0,V0,V1,1);
+% Calculate linear rate coefficient
+coeff = (V1 - V0)/tjump;
+Vapp_func = @(coeff, t) coeff(1)*t;
+
+sol_ini.par.mobseti = 0;
+% Vapp_function(sol_ini, Vapp_func, tmax, tpoints, logtime)
+jump1 = Vapp_function(sol_ini, Vapp_func, coeff, tjump, 20, 0);
 
 par.Vapp = V1;
 par.mobseti = 1;
@@ -21,26 +27,11 @@ par.tmax = tdwell;
 par.t0 = 0;%par.tmax/1e6;
 par.tmesh_type = 1;
 par.tpoints = 200;
+par.Int = Int;
 
-sol = df(jump1.dk.f, par);
+sol_relax = df(jump1, par);
 
-all_stable = verifyStabilization(sol.u, sol.t, 0.7);
-
-% loop to check ions have reached stable config- if not accelerate ions by
-% order of mag
-%{
-while any(all_stable) == 0
-    
-    % use mobseti as a multiplier- bit dangerous? But fine for now
-    par.tmax = par.tmax*10;
-    %par.t0 = par.tmax/1e6;
-    
-    sol = df(jump1.dk.f, par);
-    
-    all_stable = verifyStabilization(sol.sol, sol.t, 0.7);
-
-end
-%}
-sol_relax = sol;
+% Read out currents from LH side
+dfplot.Jt(sol_relax, 1);
 
 end
