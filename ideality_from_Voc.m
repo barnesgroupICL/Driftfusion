b36bd64d9661b798bf98d47d22546a4e91d1894d
Ilario Gelmetti <iochesonome@gmail.com>
2019-05-21 18:09:34 +0200

    ideality_from_Voc improved comments

--- ideality_from_Voc.m
+++ ideality_from_Voc.m
@@ -1,7 +1,37 @@
 function nid = ideality_from_Voc(structs_oc)
-%IDEALITY_FROM_VOC - obtains the ideality factor from a fitting of VOC versus light intensity of stabilized solutions
-% example:
+%IDEALITY_FROM_VOC - obtains the ideality factor from a fitting of VOC versus light intensity of stabilized solutions contained in a cell
+% For generating the needed input, genIntStructs can be used.
+%
+% Syntax:  nid = ideality_from_Voc(structs_oc)
+%
+% Inputs:
+%   STRUCTS_OC - a cell containing structures at open circuit and various
+%     light intensities. The open circuit can be either obtained from
+%     symmetric solutions (from genIntStructs) or from asymmetric solutions
+%     with applied voltage (from genIntStructRealVoc)
+%
+% Outputs:
+%   NID - the numeric ideality factor
+%
+% Example:
 %   nid = ideality_from_Voc(genIntStructs(ssol_i_eq_SR, 1, 0.001, 4, false))
+%     
+%
+% Other m-files required: pinana
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also pindrift.
+
+% Author: Ilario Gelmetti, Ph.D. student, perovskite photovoltaics
+% Institute of Chemical Research of Catalonia (ICIQ)
+% Research Group Prof. Emilio Palomares
+% email address: iochesonome@gmail.com
+% Supervised by: Dr. Phil Calado, Dr. Piers Barnes, Prof. Jenny Nelson
+% Imperial College London
+% October 2017; Last revision: May 2018
+
+%------------- BEGIN CODE --------------
 
 Int_array = NaN(1, size(structs_oc, 2));
 Voc_array = NaN(1, size(structs_oc, 2));
@@ -29,4 +59,5 @@ figure(1)
     ylabel('VOC')
     xlabel('log(light intensity)')
 
+%------------- END OF CODE --------------
 
