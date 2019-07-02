c9246e2a85c84aed92a22e72fc9c263ca5a3657f
Ilario Gelmetti <iochesonome@gmail.com>
2019-07-02 13:08:12 +0200

    ideality_from_dark_jVpoints add documentation header

--- ideality_from_dark_jVpoints.m
+++ ideality_from_dark_jVpoints.m
@@ -1,8 +1,37 @@
 function nid_array = ideality_from_dark_jVpoints(asymstruct_eq, Vend, deltaV)
-%IDEALITY_FROM_DARK_JVPOINTS - obtains ideality factor from stabilized points on a current-voltage curve in dark
-% https://www.pveducation.org/pvcdrom/characterisation/measurement-of-ideality-factor
-% example:
+%IDEALITY_FROM_DARK_JVPOINTS - Obtains voltage dependent ideality factor from the stabilised current at different voltages
+% A voltage dependent ideality factor can be obtained from a dark current-voltage sweep
+% (or stabilised points in the case of hysteretic devices)
+% https://www.pveducation.org/pvcdrom/characterisation/measuring-ideality-factor
+%
+% Syntax:  nid_array = ideality_from_dark_jVpoints(asymstruct_eq, Vend, deltaV)
+%
+% Inputs:
+%   ASYMSTRUCT_EQ - an asymmetric solution structure as created by PINDRIFT in dark conditions;
+%   VEND - the maximum voltage point;
+%   DELTAV - the distance between voltage points.
+%
+% Outputs:
+%   NID_ARRAY - an array of ideality factor at different applied voltages.
+%
+% Example:
 %   nid_array = ideality_from_dark_jVpoints(sol_i_eq_SR, 1, 0.05)
+%
+% Other m-files required: pindrift, pinana, stabilize
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also pindrift.
+
+% Author: Ilario Gelmetti, Ph.D. student, perovskite photovoltaics
+% Institute of Chemical Research of Catalonia (ICIQ)
+% Research Group Prof. Emilio Palomares
+% email address: iochesonome@gmail.com
+% Supervised by: Dr. Phil Calado, Dr. Piers Barnes, Prof. Jenny Nelson
+% Imperial College London
+% October 2017; Last revision: May 2018
+
+%------------- BEGIN CODE --------------
 
 p = asymstruct_eq.p;
 asymstruct_newVapp = asymstruct_eq;
@@ -64,3 +93,6 @@ function [current, asymstruct_newVapp] = IgiveCurrentForVoltage(asymstruct, Vapp
     current = Jn(end);
     
 end
+
+%------------- END OF CODE --------------
+
