409d6e7398346cf9472366ca087bb6a50fca8802
pdb13 <p.calado13@imperial.ac.uk>
2019-07-19 17:16:19 +0100

    Updated jumptoV with stabilise and ion accelerate features

--- Protocols/jumptoV.m
+++ Protocols/jumptoV.m
@@ -1,4 +1,4 @@
-function sol_relax = jumptoV(sol_ini, Vjump, tdwell, Int)
+function sol_relax = jumptoV(sol_ini, Vjump, tdwell, Int, K_cation, stabilise)
 % A function to simulate a jump-to-Voltage measurement as used for SDP
 %% Input arugments
 % SOL_INI - input solution- could be cell at equilibrium or stablised at an
@@ -11,7 +11,7 @@ function sol_relax = jumptoV(sol_ini, Vjump, tdwell, Int)
 par = sol_ini.par;
 V0 = par.Vapp;
 V1 = Vjump;
-tjump = 1e-6;   % Jump to V in 1 us
+tjump = 1e-10;   % Jump to V in 1 us
 
 % Calculate linear rate coefficient
 coeff = (V1 - V0)/tjump;
@@ -19,18 +19,37 @@ Vapp_func = @(coeff, t) coeff(1)*t;
 
 sol_ini.par.mobseti = 0;
 % Vapp_function(sol_ini, Vapp_func, tmax, tpoints, logtime)
-jump1 = Vapp_function(sol_ini, Vapp_func, coeff, tjump, 20, 0);
+jump1 = Vapp_function(sol_ini, Vapp_func, coeff, tjump, 20, 1);
 
+par.K_cation = K_cation;
 par.Vapp = V1;
 par.mobseti = 1;
 par.tmax = tdwell;
-par.t0 = 0;%par.tmax/1e6;
-par.tmesh_type = 1;
-par.tpoints = 200;
+par.t0 = par.tmax/1e8;
+par.tmesh_type = 2;
+par.tpoints = 100;
 par.Int = Int;
 
-sol_relax = df(jump1, par);
+sol = df(jump1, par);
 
+if stabilise
+    all_stable = verifyStabilization(sol.u, sol.t, 0.7);
+    % loop to check ions have reached stable config- if not accelerate ions by
+    while any(all_stable) == 0
+        disp(['increasing equilibration time, tmax = ', num2str(par.tmax*10^j)]);
+        
+        par.tmax = par.tmax*10;
+        par.t0 = par.tmax/1e6;
+        
+        sol = df(sol, par);
+        
+        all_stable = verifyStabilization(sol.u, sol.t, 0.7);
+    end
+end
+
+sol_relax = sol;
+
+sol_relax.par.K_cation = 1;
 % Read out currents from LH side
 dfplot.Jt(sol_relax, 1);
 
