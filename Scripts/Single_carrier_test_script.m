c06dbc09b9699cf7e61308f53ef40248b3f322e2
pdb13 <p.calado13@imperial.ac.uk>
2019-08-09 12:34:58 +0100

    Created single carrier test script

---
+++ Scripts/Single_carrier_test_script.m
@@ -0,0 +1,14 @@
+% Single layer test
+clear all
+close all
+% Load parameters
+par = pc('Input_files/1_layer_single_carrier.csv');
+
+% Run to equilibrium
+soleq = equilibrate(par);
+
+% Do JV scn
+JVsol = doJV(soleq.no_ion, 100e-3, 201, 1, 0, 0, 1, 1);
+
+% plot JV scan
+dfplot.JV(JVsol, 1);
