25f43e104160a8adcc9199c8e43a72c7bfb1d312
pdb13 <p.calado13@imperial.ac.uk>
2019-08-09 15:27:20 +0100

    Created master script

---
+++ Scripts/test_master_script.m
@@ -0,0 +1,14 @@
+%% Test master
+% Runs all the test scripts
+
+evidence_for_ion_script;
+
+example_script;
+
+Single_carrier_test_script;
+
+Single_layer_test_script;
+
+transient_nid_script;
+
+Vapp_function_script;
