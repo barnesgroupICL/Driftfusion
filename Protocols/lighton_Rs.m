57121545949e072f974b1fec8227d5622d4bdbf5
pdb13 <p.calado13@imperial.ac.uk>
2019-08-13 16:42:46 +0100

    Improved reliability of LIGHTONRS

--- Protocols/lighton_Rs.m
+++ Protocols/lighton_Rs.m
@@ -1,10 +1,10 @@
-function sol_ill = lighton_Rs(sol_ini, int1, stab_time, mobseti, Rs, pnts)
+function sol_ill = lighton_Rs(sol_ini, int1, stable_time, mobseti, Rs, pnts)
 % A function to switch the light on and record the transient state with a
 % series resistance Rs
 %% Input arguments
 % SOL_INI = initial conditions
 % int1 = Bias light intensity (Suns)
-% STAB_TIME = Stabilisation time - length of the transient. Setting to -1
+% stable_time = Stabilisation time - length of the transient. Setting to -1
 % to cycle to stable solution from initial time of 0.1
 % MOBSETI = Ion mobility switch
 % RS = Series resistance - recommended to use Rs = 1e6 for approx open
@@ -15,44 +15,48 @@ disp(['Starting LIGHTON, Intensity = ', num2str(int1), ' Rs = ', num2str(Rs)])
 par = sol_ini.par;
 par_origin = par;
 
-par.g1_fun_type = 'constant';
-par.tmesh_type = 2;
-par.tmax = 1e-6;
-par.t0 = par.tmax/1e6;
-par.tpoints = 10;
-par.JV = 0;
-par.mobseti = 0;
-par.Rs = Rs;
-par.Rs_initial = 1;
-
-disp('Switching on series resistance- initial fast linear sweep')
-sol_Rs = df(sol_ini, par);
-
 disp('Switching on illumination')
+par.mobseti = 0;
 par.g1_fun_type = 'constant';
 par.tmax = 1e-3;
 par.t0 = par.tmax/1e6;
 par.Rs_initial = 0;
 par.int1 = int1;
 
-sol = df(sol_Rs, par);
+sol_ill1 = df(sol_ini, par);
 
+par.g1_fun_type = 'constant';
+par.tmesh_type = 2;
+par.tmax = 1e-3;
+par.t0 = par.tmax/1e6;
+par.tpoints = 10;
+par.JV = 0;
+
+par.Rs = Rs;
+par.Rs_initial = 1;
+
+disp('Switching on series resistance- initial fast linear sweep')
+
+sol_Rs = df(sol_ill1, par);
+
+par.Rs_initial = 0;
 par.mobseti = mobseti;
 % If stabtime is entered as zero set to default value
-if stab_time >= 0
-    par.tmax = stab_time;
+if stable_time >= 0
+    par.tmax = stable_time;
 else
     par.tmax = 0.1;
 end
 par.t0 = par.tmax/1e6;
 par.tpoints = pnts;
 
-sol = df(sol, par);
+disp(['Performing initial transient with tmax = ', num2str(par.tmax)]);
+sol = df(sol_Rs, par);
 
-% Swicth off applied voltage during precondition
+% Switch off applied voltage during precondition
 par.Vapp = 0;
 
-if stab_time == -1
+if stable_time == -1
     all_stable = verifyStabilization(sol.u, sol.t, 0.7);
     
     % loop to check ions have reached stable config- if not accelerate ions by
@@ -65,7 +69,7 @@ if stab_time == -1
         par.tmax = par.tmax*10;
         par.t0 = par.tmax/1e6;
         
-        sol = df(sol, par);
+        sol = df(sol_Rs, par);
         
         all_stable = verifyStabilization(sol.u, sol.t, 0.7);
         
