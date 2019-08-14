5a96525d13c56918e90d7aef1344ce3e5cdfa0f2
pdb13 <p.calado13@imperial.ac.uk>
2019-08-14 15:22:21 +0100

    Script

--- Scripts/example_script.m
+++ Scripts/example_script.m
@@ -1,28 +1,26 @@
 % An example script for comparing the current-voltage curves for two different
 % architects of perovskite solar cell
-% clear all
-% close all
 initialise_df
 
 % Create parameters objects for Spiro/MAPI/TiO2 and PEDOT/MAPI/PCBM devices
-par.tio2 = pc('Input_files/spiro_mapi_tio2.csv');
-par.pcbm = pc('Input_files/ptpd_mapi_pcbm.csv');
+par_tio2 = pc('Input_files/spiro_mapi_tio2.csv');
+par_pcbm = pc('Input_files/ptpd_mapi_pcbm.csv');
 
 % Find equilibrium solutions
-soleq.tio2 = equilibrate(par.tio2);
-soleq.pcbm = equilibrate(par.pcbm);
+soleq_tio2 = equilibrate(par_tio2);
+soleq_pcbm = equilibrate(par_pcbm);
 
 % Perform current-voltage scans to 1.2 V
-JV.tio2.kscan50em3 = doJV(soleq.tio2.ion, 50e-3, 100, 1, 1, 0, 1.2, 3);
-JV.pcbm.kscan50em3 = doJV(soleq.pcbm.ion, 50e-3, 100, 1, 1, 0, 1.2, 3);
+JV_tio2.kscan50em3 = doJV(soleq_tio2.ion, 50e-3, 100, 1, 1, 0, 1.2, 3);
+JV_pcbm.kscan50em3 = doJV(soleq_pcbm.ion, 50e-3, 100, 1, 1, 0, 1.2, 3);
 
 % plot the current voltage curve
-dfplot.JV(JV.tio2.kscan50em3,3)
+dfplot.JV(JV_tio2.kscan50em3,3)
 
 figure(4)
 hold on
 
-dfplot.JV(JV.pcbm.kscan50em3,3)
+dfplot.JV(JV_pcbm.kscan50em3,3)
 legend('tio2-dk-f', 'tio2-dk-r', 'tio2-1sun-f', 'tio2-1sun-r',...
         'pcbm-dk-f', 'pcbm-dk-r', 'pcbm-1sun-f', 'pcbm-1sun-r')
 xlim([0, 1.2])
@@ -31,11 +29,11 @@ hold off
 
 % plot the energy level diagram and carrier densities for the tio2 device at
 % 1 V (t= 20s) during the illuminated forward scan
-dfplot.ELx(JV.tio2.kscan50em3.ill.f, 20)
+dfplot.ELx(JV_tio2.kscan50em3.ill.f, 20)
 
 % plot the currents as a function of position in the PCBM device at 0.5 V
 % (t = 10 s) during the illuminated forward scan
-dfplot.Jx(JV.pcbm.kscan50em3.ill.f, 10)
+dfplot.Jx(JV_pcbm.kscan50em3.ill.f, 10)
 
 % Save the workspace- this is commented out as the filepath should lead to
 % a folder on your computer. It is not recommended to store large files in
