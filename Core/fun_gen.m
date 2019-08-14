2122ebcda7d2c52212388895099e7b24d7356dc1
pdb13 <p.calado13@imperial.ac.uk>
2019-08-14 11:38:03 +0100

    Renamed fun_gen2 = fun_gen and deleted original

--- Core/fun_gen.m
+++ Core/fun_gen.m
@@ -1,37 +1,30 @@
-function gxt = fun_gen(gx, A, fun_type, argsin, t, tmax)
+function fun = fun_gen2(g_fun_type)
 % Function generator for generation profile
 % GX = The input profile as a function of position for 2 dimensional
 % functions
 % A = Amplitude
-% fun_type = Type of function
+% G_FUN_TYPE = Type of function
 % PAR = the parameters object
 %% 'constant'
-% ARGSIN is ignored and A is used
+% COEFF = [Amplitude]
 %% 'sweep'
-% ARGSIN = {A_initial, A}
+% COEFF = [Amplitude_initial, Amplitude_final, tmax]
 % Here A is ignored - this was the easiest way to maintain backwards
 % compatibility with different protocols and may be updated in future
 % releases
 %% 'square'
-% ARGSIN = {A_low, A_high, time_period, duty_cycle}
+% COEFF = [A_low, A_high, time_period, duty_cycle]
 %% 'sin'
-% ARGSIN = {DC_int, Delta_int, frequency}
+% COEFF = [DC_int, Delta_A, frequency, phase]
 
-switch fun_type
+switch g_fun_type
     case 'constant'
-        gxt = A*repmat(gx, length(t), 1);
+        fun = @(coeff, t) coeff(1);
     case 'sweep'
-        range = argsin;
-        for i=1:length(t)
-            gxt(i,:) = gx*(range(1) + ((range(2)-range(1))*t(i)*(1/tmax)));
-        end
+        fun = @(coeff, t) coeff(1) + (coeff(2)-coeff(1))*t/coeff(3);
     case 'square'
         % Generate intensity array
-        A_arr = argsin(1) + (argsin(2)-argsin(1))*sfg('du',t,1/argsin(3),argsin(4),0,0);
-        A_arr = A_arr';
-        gxt = A_arr*gx;
+        fun = @(coeff, t) coeff(1) + (coeff(2)-coeff(1))*lt(mod(t,coeff(3))*1/coeff(3),coeff(4)/100);
     case 'sin'
-        A_arr = argsin(1) + argsin(2)*(2*sin(2*pi*argsin(3)*t));
-        A_arr = A_arr';
-        gxt = A_arr*gx;
+        fun = @(coeff, t) coeff(1) + coeff(2)*(2*sin(2*pi*coeff(3)*t + coeff(4)));
 end
