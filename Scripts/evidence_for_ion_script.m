48a84b48c63ec9d94321d525c0de87ed72de2613
pdb13 <p.calado13@imperial.ac.uk>
2019-08-14 15:22:13 +0100

    Removed some structures from script to enable scripts to be run together without conflicts

--- Scripts/evidence_for_ion_script.m
+++ Scripts/evidence_for_ion_script.m
@@ -12,21 +12,18 @@
 % convergence of the solutions
 initialise_df
 
-clear all
-close all
-
 %% Load parameters
 % set OM =0 in pc before doing this for uniform generation in the active
 % layer
 % BC = Bottom Cathode i.e. the device with SRH recombination in the
 % contacts (interfacial recombination)
 % TC = Top Cathode i.e. device without SRH recombination
-par.tc = pc('Input_files/Evidence_for_ion_migration_noSRH.csv');
-par.bc = pc('Input_files/Evidence_for_ion_migration_SRH.csv');
+par_tc = pc('Input_files/Evidence_for_ion_migration_noSRH.csv');
+par_bc = pc('Input_files/Evidence_for_ion_migration_SRH.csv');
 
 % Get equilibrium solutions
-soleq.bc = equilibrate(par.bc);
-soleq.tc = equilibrate(par.tc);
+soleq_bc = equilibrate(par_bc);
+soleq_tc = equilibrate(par_tc);
 
 %% Current-voltage scans
 
@@ -35,7 +32,7 @@ soleq.tc = equilibrate(par.tc);
 % for the JV: doJV(sol_ini, JVscan_rate, JVscan_pnts, Intensity, mobseti, Vstart, Vend, option)
 % Slow scan with ion mobility set to 1 cm2V-1s-1 (by setting mobseti =
 % 1e10)- this ensure ions are in in equilibrium throughout scan
-JV.bc_0V_to_m1V = doJV(soleq.bc.ion, 1e-6, 40, 0, 1e10, 0, -1, 1);
+JV.bc_0V_to_m1V = doJV(soleq_bc.ion, 1e-6, 40, 0, 1e10, 0, -1, 1);
 % Scan from -1.0 V to 1.2 V dark and light - ions must be accelerated by
 % x10 for convergence
 JV.bc_m1V_to_1p2V = doJV(JV.bc_0V_to_m1V.dk.f, 40, 200, 1, 10, -1, 1.2, 3);
@@ -43,7 +40,7 @@ JV.bc_m1V_to_1p2V = doJV(JV.bc_0V_to_m1V.dk.f, 40, 200, 1, 10, -1, 1.2, 3);
 %% Top cathode device
 % Scan to -1.0 V with ion mobility off- dark only to get starting condition
 % for the JV
-JV.tc_0V_to_m1V = doJV(soleq.tc.ion, 1e-6, 40, 0, 1e10, 0, -1, 1);
+JV.tc_0V_to_m1V = doJV(soleq_tc.ion, 1e-6, 40, 0, 1e10, 0, -1, 1);
 % Scan from -1.0 V to 1.4 V dark and light
 JV.tc_m1V_to_1p2V = doJV(JV.tc_0V_to_m1V.dk.f, 40, 200, 1, 10, -1, 1.2, 3);
 
@@ -58,17 +55,17 @@ hold off
 %% Open circuit voltage tranisents
 % Using Rs = 1 MOhm rather than the mirrored cell approach.
 % lighton_Rs(sol_ini, Int, stab_time, mobseti, Rs, pnts)
-Voc_transient.bc = lighton_Rs(soleq.bc.ion, 1, 1, 1, 1e6, 400);
-Voc_transient.tc = lighton_Rs(soleq.tc.ion, 1, 1, 1, 1e6, 400);
+Voc_transient_bc = lighton_Rs(soleq_bc.ion, 1, 1, 1, 1e6, 400);
+Voc_transient_tc = lighton_Rs(soleq_tc.ion, 1, 1, 1, 1e6, 400);
 
 % Plot the Voc transients
-dfplot.Voct(Voc_transient.bc);
+dfplot.Voct(Voc_transient_bc);
 hold on
-dfplot.Voct(Voc_transient.tc);
+dfplot.Voct(Voc_transient_tc);
 hold off
 
 % Plot the EL diagrams at 1 ms and 1 sec
-dfplot.ELx(Voc_transient.bc, [1e-3, 1])
+dfplot.ELx(Voc_transient_bc, [1e-3, 1])
 % Uncomment to plot EL and carrier density diagrams for Top Cathode device
 %dfplot.ELx(Voc_transient.tc, [1e-3, 1])
 
