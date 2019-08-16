8ffb8eb545ead083bfd6346ec7a593ecce79258d
pdb13 <p.calado13@imperial.ac.uk>
2019-08-16 10:24:33 +0100

    Renamed solutions in scripts to prevent conflicts when testing

--- Scripts/IMVS_IMPS_script.m
+++ Scripts/IMVS_IMPS_script.m
@@ -15,10 +15,4 @@ sol_IMVS = do_IMVS(soleq_3l.ion, 1, 0.2, 4, 1, 200);
 sol_IMPS = do_IMPS(soleq_3l.ion, 0, 0.02, 4, 1, 200);
 
 %% Plot the generation rate as a function of x and t
-x_ihalf = getvarihalf(sol_IMVS.x);
-
-figure(200)
-surf(x_ihalf, sol_IMVS.t, sol_IMVS.g)
-xlabel('Position [cm]')
-ylabel('Time [s]')
-zlabel('Generation rate [cm-3s-1]')
+dfplot.gxt(sol_IMVS)
--- Scripts/transient_nid_script.m
+++ Scripts/transient_nid_script.m
@@ -1,19 +1,19 @@
 %% A script to obtain the transient ideality factor
-clear all
-close all
+% clear all
+% close all
 
 %% Insert the appropriate file path here
-par = pc('Input_files/3_layer_test.csv');
+par_nid = pc('Input_files/3_layer_test.csv');
 
 %% Get equilibrium
-soleq = equilibrate(par);
+soleq_nid = equilibrate(par_nid);
 
 %% Preconditioning at Vbi+0.2 V until stable- change the second argument for a different precondition
 %           jumptoV(sol_ini, Vjump, tdwell, mobseti, Int, stabilise, accelerate)
-sol_relax = jumptoV(soleq.ion, par.Vbi+0.2, 1e-3, 1, 0, 1, 1);
+sol_jump = jumptoV(soleq_nid.ion, par_nid.Vbi+0.2, 1e-3, 1, 0, 1, 1);
 
 %% Get open circuit solutions
-sol_OC = transient_nid(sol_relax, logspace(-4,2,7), 10, 1, 1e6, 200);
+sol_OC = transient_nid(sol_jump, logspace(-3,2,6), 10, 1, 1e6, 200);
 
 %% Analyse the solution
 nidt_mean = transient_nid_ana(sol_OC);
