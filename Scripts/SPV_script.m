fd3a1535fdd02b7caa6e3ee237979be9156df0cb
pdb13 <p.calado13@imperial.ac.uk>
2019-09-23 14:45:32 +0100

    Updated SPV script

--- Scripts/SPV_script.m
+++ Scripts/SPV_script.m
@@ -7,12 +7,11 @@ initialise_df
 
 %% Create a parameters object
 % A parameters object is created by calling the parameters class PC. In
-% this instance I am calling the object PAR.TIO2 which is a neat way to
-% store things.
+% this instance I am calling the object PAR_TIO2_2L.
 % To input a different parameter set duplicate SPV_TiO2_MAPI_bilayer.csv,
 % open it in Excel and then edit the parameters. Save it as a different
 % name and then use that as the file in the path of the input argument to
-% PC (the parameters class) as I have done below.
+% PC (the parameters class):
 par_tio2_2l = pc('input_files/SPV_TiO2_MAPI_bilayer.csv');
 
 %% Get equilibrium solutions
@@ -22,21 +21,20 @@ par_tio2_2l = pc('input_files/SPV_TiO2_MAPI_bilayer.csv');
 % of Driftfusion we solve for carrier densities as opposed to Fermi levels. 
 % Since the hole densities are effectively zero in the dark it's safe to 
 % ignore them since they do not contribute to currents or the electrostatic 
-% potential significantly. The residual currents at equilibrium arise from 
-% numerical problems with the bilayer configuration. This can be an issue
-% in some instances but a workaround is contained in DOSPV
+% potential significantly.
 soleq_tio2_2l = equilibrate(par_tio2_2l);
 
 %% Do the SPV procedure
-% SOLEQ.TIO2 contains 2 solutions, with and without ions. Here we will use
-% the solution SOLEQ.TIO2.ION as the input to DOSPV which can be found in
+% SOLEQ_TIO2_2L contains 2 solutions, with and without ions. Here we will use
+% the solution SOLEQ_TIO2_2L.ION as the input to DOSPV which can be found in
 % the PROTOCOLS folder. The code is commented with the information you need
 % regarding the input arguments. The code splits the solution into 2 parts-
 % the illuminated step and the dark step, SPVSOL.ILL and SPVSOL.DK.
+% Input arguments: dospv(sol_ini, Int, mobseti, tpoints, tmax, Rs, stabilise)
 spvsol = dospv(soleq_tio2_2l.ion, 0.2, 1, 200, 10, 1e6, 0);
 
 %% Analyse the solution
-% SPVANA5 takes the two solutions contained in SPVSOL, extracts the charge
+% SPVANA (in the Analysis folder) takes the two solutions contained in SPVSOL, extracts the charge
 % density, electric field, and potential at the right hand boundary as a 
 % function of time the plots them. The outputs can be store in the output 
 % structure SPVDAT (although this can be called anything).
