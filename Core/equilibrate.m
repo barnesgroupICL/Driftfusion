7af216243d222cf2f91c11d8d9553023ae5bf565
pdb13 <p.calado13@imperial.ac.uk>
2019-09-24 16:05:31 +0100

    changed name .no_ion to .el

--- Core/equilibrate.m
+++ Core/equilibrate.m
@@ -102,7 +102,7 @@ par.SRHset = 1;
 par.tmax = 1e-6;
 par.t0 = par.tmax/1e3;
 
-soleq.no_ion = df(soleq_nosrh, par);
+soleq.el = df(soleq_nosrh, par);
 disp('Complete')
 
 %% Equilibrium solutions with ion mobility switched on
