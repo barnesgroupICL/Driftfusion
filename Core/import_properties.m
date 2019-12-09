f2aa2fc7f81bd0cb0605649b0466909f381070bb
pdb13 <p.calado13@imperial.ac.uk>
2019-12-09 12:33:24 +0000

    Sorted out IMPORT_PROPERTIES such that all variables are now checked and warnings given if theya re absent.

--- Core/import_properties.m
+++ Core/import_properties.m
@@ -1,110 +1,209 @@
 function par = import_properties(par, filepath)
-            % A function to overwrite the properties in par with those imported from a
-            % text file located in filepath
-            T = readtable(filepath{1,1});
+% A function to IMPORT_PROPERTIES from a text file LOCATED at FILEPATH. Each of the listed properties
+% is checked to see if it is available in the .CSV file. If it is available, the existing properties
+% are overwritten otherwise a warning is displayed.
 
-            par.layer_type = T{:,'layer_type'}';
-            par.stack = T{:,'stack'}';
-            par.dcell = T{:, 'thickness'}';
-            par.pcell = T{:, 'points'}';
-            par.EA = T{:, 'EA'}';
-            par.IP = T{:, 'IP'}';
-            par.E0 = T{:, 'E0'}';
+T = readtable(filepath{1,1});   % Reads-in in the external .CSV file to a table T
 
-            par.Nc = T{:, 'Nc'}';
-            par.Nv = T{:, 'Nv'}';
-            try
-                par.Nani = T{:, 'Nion'}';   % Backward compatibility
-            end
-            try
-                par.Nani = T{:, 'Nani'}';
-            end
-            par.Ncat = T{:, 'Ncat'}';
-            try
-                par.DOSani = T{:, 'DOSion'}';   % backwards compatibility
-            end
-            try
-                par.DOSani = T{:, 'DOSani'}';
-            end
-            par.DOScat = T{:, 'DOScat'}';
-            par.mue = T{:, 'mue'}';
-            par.muh = T{:, 'muh'}';
-            try
-                par.muani = T{:, 'muion'}';
-            end
+try
+    par.layer_type = T{:,'layer_type'}';
+catch
+    warning('No layer type (layer_type) defined in .csv . Using default in PC')
+end
+try
+    par.stack = T{:,'stack'}';
+catch
+    warning('No stack (stack) defined in .csv . Using default in PC')
+end
+try
+    par.dcell = T{:, 'thickness'}';
+catch
+    warning('No thickness array (thickness) defined in .csv . Using default in PC')
+end
+try
+    par.pcell = T{:, 'points'}';
+catch
+    warning('No layer points array (points) defined in .csv . Using default in PC')
+end
+try
+    par.EA = T{:, 'EA'}';
+catch
+    warning('No electron affinity array (EA) defined in .csv . Using default in PC')
+end
+try
+    par.IP = T{:, 'IP'}';
+catch
+    warning('No ionisation potential array (IP) defined in .csv . Using default in PC')
+end
+try
+    par.E0 = T{:, 'E0'}';
+catch
+    warning('No equilibrium Fermi level array (E0) defined in .csv . Using default in PC')
+end
+try
+    par.Nc = T{:, 'Nc'}';
+catch
+    warning('No conduction band eDOS array (Nc) defined in .csv . Using default in PC')
+end
+try
+    par.Nv = T{:, 'Nv'}';
+catch
+    warning('No valence band eDOS array (Nv) defined in .csv . Using default in PC')
+end
+try
+    par.Nani = T{:, 'Nani'}';
+catch
+    warning('No equilibrium anion density array (Nani) defined in .csv . Using default in PC')
+end
+try
+    par.Ncat = T{:, 'Ncat'}';
+catch
+    warning('No equilibrium cation density array (Ncat) defined in .csv . Using default in PC')
+end
+try
+    par.DOSani = T{:, 'DOSani'}';
+catch
+    warning('No maximum anion density array (DOSani) defined in .csv . Using default in PC')
+end
+try
+    par.DOScat = T{:, 'DOScat'}';
+catch
+    warning('No maximum cation density array (DOScat) defined in .csv . Using default in PC')
+end
+try
+    par.mue = T{:, 'mue'}';
+catch
+    warning('No electron mobility (mue) defined in .csv . Using default in PC')
+end
+try
+    par.muh = T{:, 'muh'}';
+catch
+    warning('No hole mobility (muh) defined in .csv . Using default in PC')
+end
 
-            try
-                par.muani = T{:, 'muani'}';
-            end
-            par.mucat = T{:, 'mucat'}';
-            par.epp = T{:, 'epp'}';
+try
+    par.muani = T{:, 'muani'}';
+catch
+    warning('No anion mobility (muani) defined in .csv . Using default in PC')
+end
+try
+    par.mucat = T{:, 'mucat'}';
+catch
+    warning('No cation mobility (mucat) defined in .csv . Using default in PC')
+end
+try
+    par.epp = T{:, 'epp'}';
+catch
+    warning('No relative dielectric constant (epp) defined in .csv . Using default in PC')
+end
 
-            try
-                par.g0 = T{:, 'G0'}';   % For backwards compatibility
-            end
+try
+    par.g0 = T{:, 'g0'}';
+catch
+    warning('No uniform generation rate (g0) defined in .csv . Using default in PC')
+end
+try
+    par.krad = T{:, 'krad'}';
+catch
+    warning('No radiative recombinaiton coefficient array (krad) defined in .csv . Using default in PC')
+end
 
-            try
-                par.g0 = T{:, 'g0'}';
-            end
+try
+    par.taun = T{:, 'taun'}';
+catch
+    warning('No SRH electron lifetime array (taun) defined in .csv . Using default in PC')
+end
+try
+    par.taup = T{:, 'taup'}';
+catch
+    warning('No SRH hole lifetime array (taup) defined in .csv . Using default in PC')
+end
+try
+    par.sn_l = T{1, 'sn_l'}';
+catch
+    warning('No sn_l defined in .csv . Using default in PC')
+end
+try
+    par.sp_l = T{1, 'sp_l'}';
+catch
+    warning('No sp_l defined in .csv . Using default in PC')
+end
+try
+    par.sn_r = T{1, 'sn_r'}';
+catch
+    warning('No sn_r defined in .csv . Using default in PC')
+end
+try
+    par.sp_r = T{1, 'sp_r'}';
+catch
+    warning('No sp_r defined in .csv . Using default in PC')
+end
 
-            par.krad = T{:, 'krad'}';
-            par.taun = T{:, 'taun_SRH'}';
-            par.taup = T{:, 'taup_SRH'}';
-            par.sn_l = T{1, 'sn_l'}';
-            par.sp_l = T{1, 'sp_l'}';
-            par.sn_r = T{1, 'sn_r'}';
-            par.sp_r = T{1, 'sp_r'}';
+try
+    par.Phi_left = T{1, 'Phi_left'};
+catch
+    warning('No Phi_left defined in .csv . Using default in PC')
+end
+try
+    par.Phi_right = T{1, 'Phi_right'};
+catch
+    warning('No Phi_right defined in .csv . Using default in PC')
+end
+try
+    par.Et = T{:,'Et'}';
+catch
+    warning('No trap energy array (Et) defined in .csv . Using default in PC')
+end
+try
+    par.OM = T{1, 'OM'};
+catch
+    warning('No optical model (OM) specified in .csv Using default in PC')
+end
+try
+    par.xmesh_type = T{1, 'xmesh_type'};
+catch
+    warning('No spatial mesh type (xmesh_type) defined in .csv . Using default in PC')
+end
+try
+    par.side = T{1, 'side'};
+catch
+    warning('Illumination side (side) undefined in .csv . Using default in PC')
+end
+try
+    par.N_ionic_species = T{1, 'N_ionic_species'};
+catch
+    warning('No of ionic species (N_ionic_species) undefined in .csv. Using default in PC')
+end
 
-            try
-                par.Phi_left = T{1, 'PhiA'};
-            end
-
-            try
-                par.Phi_left = T{1, 'Phi_left'};    % For backwards compatibility
-            end
-
-            try
-                par.Phi_right = T{1, 'PhiC'};       % For backwards compatibility
-            end
-
-            try
-                par.Phi_right = T{1, 'Phi_right'};
-            end
-            
-            try
-                par.Rs = T{1, 'Rs'};
-            end
-            
-            try
-                par.Et = T{:,'Et'}';
-            end
-
-            try
-                par.Et = T{:,'Et_bulk'}';   % For backwards compatibility
-            end
-
-            try
-                par.OM = T{1, 'OM'};
-            catch
-                warning('No optical model (OM) specified in .csv Using default in PC')
-            end
-
-            try
-                par.xmesh_type = T{1, 'xmesh_type'};
-            catch
-                warning('No spatial mesh type (xmesh_type) defined in .csv . Using default in PC')
-            end
-
-            try
-                par.side = T{1, 'side'};
-            catch
-                warning('Illumination side (side) undefined in .csv . Using default in PC')
-            end
-
-            try
-                par.N_ionic_species = T{1, 'N_ionic_species'};
-            catch
-                warning('No of ionic species (N_ionic_species) undefined in .csv. Using default in PC')
-            end
+%% Additional checks for backwards compatibility- old variable names
+% No warning required otherwise will constantly warn when building
+% parameters object
+try
+    par.Nani = T{:, 'Nion'}';
+end
+try
+    par.DOSani = T{:, 'DOSion'}';
+end
+try
+    par.muani = T{:, 'muion'}';
+end
+try
+    par.Et = T{:,'Et_bulk'}';
+end
+try
+    par.taun = T{:, 'taun_SRH'}';
+end
+try
+    par.taup = T{:, 'taup_SRH'}';
+end
+try
+    par.g0 = T{:, 'G0'}';
+end
+try
+    par.Phi_left = T{1, 'PhiA'};
+end
+try
+    par.Phi_right = T{1, 'PhiC'}; 
+end
 
 end
