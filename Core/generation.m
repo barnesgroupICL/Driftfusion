77fe9b96800307622ac87ace396da8ee88831639
pdb13 <p.calado13@imperial.ac.uk>
2019-12-11 15:21:58 +0000

    moved generation

--- Core/generation.m
+++ Core/generation.m
@@ -1,12 +1,14 @@
 function gx = generation(par, source_type, laserlambda)
-% This function calls the correct funciton to calculate generation profiles as a function of position
+% This function calls the appropriate function (based on the chosen optical model) to calculate
+% generation profiles as a function of position
 % SOURCE_TYPE = either 'AM15' or 'laser'
 % LASERLAMBDA = Laser wavelength - ignored if SOURCE_TYPE = AM15
 
 xsolver = par.x_ihalf;
 switch par.OM
     case 0
-        gx = getvarihalf(par.dev.g0);    % This currently results in the generation profile being stored twice and could be optimised
+        % This currently results in the generation profile being stored twice and could be optimised
+        gx = build_property(par.g0, xsolver, par, 'zeroed', 0);    
     case 1
         % beerlambert(par, x, source_type, laserlambda, figson)
         gx = beerlambert(par, par.xx, source_type, laserlambda, 0);
