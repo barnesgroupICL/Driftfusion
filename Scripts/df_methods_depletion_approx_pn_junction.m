7405f1a8e30baa34d7ad82ee3b287fa2bccbe76b
pdb13 <p.calado13@imperial.ac.uk>
2019-12-12 14:47:03 +0000

    fixed plot

--- Scripts/df_methods_depletion_approx_pn_junction.m
+++ Scripts/df_methods_depletion_approx_pn_junction.m
@@ -1,10 +1,14 @@
 initialise_df
 
+%% Import pn junction properties from external .csv file
 par_pn = pc('Input_files/pn_junction_DA_compare.csv');
 
+%% Obtain equilibrium solution
 soleq_pn = equilibrate(par_pn);
-dfplot.ELx(soleq);
 
+%% Current voltage scans
 % JV = doJV(sol_ini, JVscan_rate, JVscan_pnts, Intensity, mobseti, Vstart, Vend, option)
-JV = doJV(soleq, 1, 241, 1, 0, 0, 1.1, 3);
+JV_pn = doJV(soleq_pn.el, 1, 241, 1, 0, 0, 1.1, 3);
 
+%% Plot equilibrium
+dfplot.ELnpx(soleq_pn.el);
