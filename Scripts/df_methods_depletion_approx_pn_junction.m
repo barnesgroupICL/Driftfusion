8dd8f7d1aab3d47813ec8388988509b628f13450
pdb13 <p.calado13@imperial.ac.uk>
2019-12-16 17:51:07 +0000

    Updated call to doJV to include option2

--- Scripts/df_methods_depletion_approx_pn_junction.m
+++ Scripts/df_methods_depletion_approx_pn_junction.m
@@ -6,8 +6,9 @@ par_pn_base = pc('Input_files/pn_junction_DA_compare.csv');
 
 soleq_base = equilibrate_short(par_pn_base);
 
+%% Base parameters current-voltage
 % JV = doJV(sol_ini, JVscan_rate, JVscan_pnts, Intensity, mobseti, Vstart, Vend, option)
-JV_base = doJV(soleq_base, 1e-9, 241, 1, 0, 0, 0.8, 3);
+JV_base = doJV(soleq_base, 1e-9, 241, 1, 0, 0, 0.8, 3, 1);
 
 dfplot.JV(JV_base,3);
 hold on
@@ -15,7 +16,6 @@ ylim([-60e-3, 10e-3])
 
 % Get depletion approximation solution
 [JV_DA.base, r0_DA.base, Voc_DA.base, g0_DA.base] = calcR0_pn_junction(par_pn_base);
-
 
 %% tau = 1e-7 s
 par_pn_tau1em7 = par_pn_base;
@@ -27,7 +27,7 @@ par_pn_tau1em7 = refresh_device(par_pn_tau1em7);
 
 soleq_tau1em7 = equilibrate_short(par_pn_tau1em7);
 
-JV_tau1em7 = doJV_short(soleq_tau1em7, 1e-9, 241, 1, 0, 0, 0.8, 3);
+JV_tau1em7 = doJV_short(soleq_tau1em7, 1e-9, 241, 1, 0, 0, 0.8, 3, 1);
 
 dfplot.JV(JV_tau1em7,3);
 hold on
@@ -46,7 +46,7 @@ par_pn_tau1em8 = refresh_device(par_pn_tau1em8);
 
 soleq_tau1em8 = equilibrate_short(par_pn_tau1em8);
 
-JV_tau1em8 = doJV_short(soleq_tau1em8, 1e-9, 241, 1, 0, 0, 0.8, 3);
+JV_tau1em8 = doJV_short(soleq_tau1em8, 1e-9, 241, 1, 0, 0, 0.8, 3, 1);
 
 dfplot.JV(JV_tau1em8,3);
 hold on
