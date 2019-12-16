4026a986320e6e414112925e7ad474f58cdc1581
pdb13 <p.calado13@imperial.ac.uk>
2019-12-16 16:42:39 +0000

    Script

--- Scripts/df_methods_depletion_approx_pn_junction.m
+++ Scripts/df_methods_depletion_approx_pn_junction.m
@@ -16,24 +16,6 @@ ylim([-60e-3, 10e-3])
 % Get depletion approximation solution
 [JV_DA.base, r0_DA.base, Voc_DA.base, g0_DA.base] = calcR0_pn_junction(par_pn_base);
 
-%% tau = 1e-5 s
-par_pn_tau1em5 = par_pn_base;
-
-par_pn_tau1em5.taun(1) = 1e-5;
-par_pn_tau1em5.taup(end) = 1e-5;
-
-par_pn_tau1em5 = refresh_device(par_pn_tau1em5);
-
-soleq_tau1em5 = equilibrate_short(par_pn_tau1em5);
-
-JV_tau1em5 = doJV_short(soleq_tau1em5, 1e-9, 241, 1, 0, 0, 0.8, 3);
-
-dfplot.JV(JV_tau1em5,3);
-hold on
-ylim([-60e-3, 10e-3])
-
-% Get depletion approximation solution
-[JV_DA.tau1em5, r0_DA.tau1em5, Voc_DA.tau1em5, g0_DA.tau1em5] = calcR0_pn_junction(par_pn_tau1em5);
 
 %% tau = 1e-7 s
 par_pn_tau1em7 = par_pn_base;
@@ -53,3 +35,22 @@ ylim([-60e-3, 10e-3])
 
 % Get depletion approximation solution
 [JV_DA.tau1em7, r0_DA.tau1em7, Voc_DA.tau1em7, g0_DA.tau1em7] =  calcR0_pn_junction(par_pn_tau1em7);
+
+%% tau = 1e-8 s
+par_pn_tau1em8 = par_pn_base;
+
+par_pn_tau1em8.taun(1) = 1e-5;
+par_pn_tau1em8.taup(end) = 1e-5;
+
+par_pn_tau1em8 = refresh_device(par_pn_tau1em8);
+
+soleq_tau1em8 = equilibrate_short(par_pn_tau1em8);
+
+JV_tau1em8 = doJV_short(soleq_tau1em8, 1e-9, 241, 1, 0, 0, 0.8, 3);
+
+dfplot.JV(JV_tau1em8,3);
+hold on
+ylim([-60e-3, 10e-3])
+
+% Get depletion approximation solution
+[JV_DA.tau1em8, r0_DA.tau1em8, Voc_DA.tau1em8, g0_DA.tau1em8] = calcR0_pn_junction(par_pn_tau1em8);
