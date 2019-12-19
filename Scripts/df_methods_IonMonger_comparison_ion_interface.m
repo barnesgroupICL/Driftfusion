e269473e52b6c035b123a9f573bb481b2ff2a58b
pdb13 <p.calado13@imperial.ac.uk>
2019-12-19 17:29:07 +0000

    fixes to IonMonger comparison

--- Scripts/df_methods_IonMonger_comparison_ion_interface.m
+++ Scripts/df_methods_IonMonger_comparison_ion_interface.m
@@ -4,7 +4,7 @@ initialise_df
 par_df_im = pc('Input_files/IonMonger_default_noIR.csv');
 
 %% Get equilibrium solutions
-soleq_df_im_0p85 = equilibrate(par_df_im);
+soleq_df_im = equilibrate(par_df_im);
 
 %% Perform JV scan at 1.2 Vs-1
 %       doJV(sol_ini, JVscan_rate, JVscan_pnts, Intensity, mobseti, Vstart, Vend, option)
@@ -13,11 +13,18 @@ JV_df_1V_im = doJV(soleq_df_im.ion, 1, 131, 1.0398, 1, 0, -1.3, 2);
 JV_df_10V = doJV(soleq_df.ion, 10, 131, 1.0398, 1, 0, -1.3, 2);
 
 %% Plot JV
-dfplot.JV(JV_df_1V_im, 2);
+dfplot.JV(JV_df_0p1V_im, 3);
 hold on
+dfplot.JV(JV_df_1V_im, 3);
+hold on
+dfplot.JV(JV_df_10V_im, 3);
 ylim([-10e-3,25e-3])
 
-% %% Electrostatic potential during forward scan
+%% Electrostatic potential during forward scan
+
+dfplot.Vx(JV_df_1V_im.ill.f, [JV_df_1V_im.ill.f.t(1), JV_df_1V_im.ill.f.t(21),...
+    JV_df_1V_im.ill.f.t(41), JV_df_1V_im.ill.f.t(61), JV_df_1V_im.ill.f.t(81)]);
+
 % dfplot.Vx(JV_df_1V_im.ill.f, [JV_df_1V_im.ill.f.t(1), JV_df_1V_im.ill.f.t(21),...
 %     JV_df_1V_im.ill.f.t(41), JV_df_1V_im.ill.f.t(61), JV_df_1V_im.ill.f.t(81)]);
 % 
