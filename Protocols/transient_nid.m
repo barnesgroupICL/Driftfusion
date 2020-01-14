215280e647eba002620db0d4e07a1a1bc7e7ff88
pdb13 <p.calado13@imperial.ac.uk>
2020-01-14 09:14:27 +0000

    Added comments to transientnid

--- Protocols/transient_nid.m
+++ Protocols/transient_nid.m
@@ -1,5 +1,7 @@
 function sol_OC = transient_nid(sol_ini, int_arr, stab_time, mobseti, Rs, pnts)
-
+% Performs a transient ideality factor simulation. 
+% See Calado 2019, Physical Review Applied for details of the measurement protocol.
+% Input arguments:
 % SOL_INI = initial conditions
 % INT_ARR = An array of light intensities (Suns)
 % STAB_TIME = Stabilisation time - length of the transient. Setting to -1
@@ -8,6 +10,7 @@ function sol_OC = transient_nid(sol_ini, int_arr, stab_time, mobseti, Rs, pnts)
 % RS = Series resistance - recommended to use Rs = 1e6 for approx open
 % circuit
 % PNTS = Number of time points in the solution
+% P. Calado, 2019, Imperial College London
 
 for i = 1:length(int_arr)
     sol_OC(i) = lighton_Rs(sol_ini, int_arr(i), stab_time, mobseti, Rs, pnts);
