bb676563758c9d6c8a378a31a60ab51714e308ad
pdb13 <p.calado13@imperial.ac.uk>
2020-03-24 12:34:08 +0000

    Renamed krad Bdir

--- Core/dfana.m
+++ Core/dfana.m
@@ -343,14 +343,14 @@ classdef dfana
             % Property matrices
             eppmat = repmat(dev.epp, length(t), 1);
             nimat = repmat(dev.ni, length(t), 1);
-            kradmat = repmat(dev.krad, length(t), 1);
+            Bdirmat = repmat(dev.Bdir, length(t), 1);
             taunmat = repmat(dev.taun, length(t), 1);
             taupmat = repmat(dev.taup, length(t), 1);
             ntmat = repmat(dev.nt, length(t), 1);
             ptmat = repmat(dev.pt, length(t), 1);
             
             % Recombination
-            r.btb = kradmat.*(n.*p - nimat.^2);
+            r.btb = Bdirmat.*(n.*p - nimat.^2);
             
             r.srh = ((n.*p - nimat.^2)./((taunmat.*(p+ptmat)) + (taupmat.*(n+ntmat))));
             
@@ -374,14 +374,14 @@ classdef dfana
             % Property matrices
             eppmat = repmat(devihalf.epp, length(t), 1);
             nimat = repmat(devihalf.ni, length(t), 1);
-            kradmat = repmat(devihalf.krad, length(t), 1);
+            Bdirmat = repmat(devihalf.Bdir, length(t), 1);
             taunmat = repmat(devihalf.taun, length(t), 1);
             taupmat = repmat(devihalf.taup, length(t), 1);
             ntmat = repmat(devihalf.nt, length(t), 1);
             ptmat = repmat(devihalf.pt, length(t), 1);
             
             % Recombination
-            r.btb = kradmat.*(n_ihalf.*p_ihalf - nimat.^2);
+            r.btb = Bdirmat.*(n_ihalf.*p_ihalf - nimat.^2);
             
             r.srh = ((n_ihalf.*p_ihalf - nimat.^2)./((taunmat.*(p_ihalf+ptmat)) + (taupmat.*(n_ihalf+ntmat))));
             
@@ -694,8 +694,8 @@ classdef dfana
         function value = PLt(sol)
                 [u,t,x,par,dev,n,p,a,c,V] = dfana.splitsol(sol);
                 
-                kradmat = dev.krad;
-                value = trapz(x,(dev.krad.*(n.*p-dev.ni.^2)),2);
+                Bdirmat = dev.Bdir;
+                value = trapz(x,(dev.Bdir.*(n.*p-dev.ni.^2)),2);
         end
         
         function VQFL = calcVQFL(sol)
