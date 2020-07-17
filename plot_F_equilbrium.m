4940f095e3f0b6a66f24d6fdd61bbb0ebe8846db
pdb13 <p.calado13@imperial.ac.uk>
2020-07-17 15:46:24 +0100

    script

---
+++ plot_F_equilbrium.m
@@ -0,0 +1,7 @@
+
+%% Intrinsic
+dfplot.Fx(soleq.intrinsic.el)
+hold on
+dfplot.Fx(soleq.intrinsic.ion)
+legend('HTL','int1','Active','int2','ETL','Intrinsic','Intrinsic with Shottky Defects')
+hold off
