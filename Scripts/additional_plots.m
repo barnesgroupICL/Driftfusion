4251e769597a5944a8e4fa2375077c2202a75153
pdb13 <p.calado13@imperial.ac.uk>
2020-09-02 17:09:36 +0100

    New plot script for absolute difference

---
+++ Scripts/additional_plots.m
@@ -0,0 +1,12 @@
+%% Additional plots for comparison with ASA
+% Note: Data stored locally- not available in the repository
+
+semilogy(DF_Vapp, abs(dk_error_percent_m1a),...
+    DF_Vapp, abs(dk_error_percent_m1b),...
+    DF_Vapp, abs(dk_error_percent_m2a),...
+    DF_Vapp, abs(dk_error_percent_m2b))
+xlabel('Applied voltage [V]')
+ylabel('Absolute difference [%]')
+xlim([0, 1.4])
+%ylim([0, 20])
+legend('PS 1a', 'PS 1b', 'PS 2a', 'PS 2b')
