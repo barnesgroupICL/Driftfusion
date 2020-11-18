d0323af818de2afdefe1676991ab8d8c2e99a860
pdb13 <p.calado13@imperial.ac.uk>
2020-11-18 17:27:33 +0000

    Added initialise_df command to master script

--- CPE_workshop/CPE_workshop_script.m
+++ CPE_workshop/CPE_workshop_script.m
@@ -3,16 +3,19 @@
 % Authors: P.R.F. Barnes, P. Calado 2020
 % Department of Physics, Imperial College London
 
+%% Initialise the code. Only needs to be run once at the start of each session
+initialise_df
+
 %% Input parameters
 params_filepath = './CPE_workshop_Input_files/blocking_contacts_3_layer.csv';   % Filepath to the parameters file
-output_filename = 'blocking_contacts_3_layer';  % Filename for output file
+output_filename = 'blocking_contacts_3_layer';      % Filename for output file
 
-light_intensities = [0, 1, 2, 3, 4, 5, 6];      % List the light intensities here
-Vmax = 1.2;                                       % Maximum voltage for cyclic voltammogram
-Vmin = -1.2;                                      % Minimum voltage for cyclic voltammogram
+light_intensities = [0, 1, 2, 3, 4, 5, 6];          % List the light intensities here
+Vmax = 1.2;                                         % Maximum voltage for cyclic voltammogram
+Vmin = -1.2;                                        % Minimum voltage for cyclic voltammogram
 
-xlimits = [0, 0];   % Sets the limits for the CV plot x-axis. Set to [0 ,0] for autoscaling.
-ylimits = [0, 0];   % Sets the limits for the CV plot y-axis. Set to [0 ,0] for autoscaling.
+xlimits = [0, 0];   % Sets the limits for the CV plot x-axis. Set to [0 ,0] for autoscaling
+ylimits = [0, 0];   % Sets the limits for the CV plot y-axis. Set to [0 ,0] for autoscaling
 
 %% Call function to obtain equilibrium and cyclic voltammogram solutions
 sol = sigma_light(params_filepath, light_intensities, Vmax, Vmin, output_filename);
