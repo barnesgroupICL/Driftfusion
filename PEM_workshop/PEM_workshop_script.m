35f1bc1e7a8b2545f0ca2b506891a084da5be0bb
pdb13 <p.calado13@imperial.ac.uk>
2020-11-24 22:17:35 +0000

    Final update prep

--- PEM_workshop/PEM_workshop_script.m
+++ PEM_workshop/PEM_workshop_script.m
@@ -7,10 +7,9 @@
 initialise_df
 
 %% Input parameters
-params_filepath = './PEM_workshop_Input_files/MIS.csv';     % Filepath to the parameters file
-output_filename = 'MIS';                                    % Filename for output file
-
-light_intensities = 0;          % List the light intensities here
+params_filepath = './PEM_workshop_Input_files/intrinsic_ohmic.csv';     % Filepath to the parameters file
+output_filename = 'intrinsic_ohmic';                                    % Filename for output file
+light_intensities = [0];        % List the light intensities here in square brackets e.g. [0, 1, 2]
 Vmax = 1.2;                     % Maximum voltage for cyclic voltammogram
 Vmin = -1.2;                    % Minimum voltage for cyclic voltammogram
 scan_rate = 1e-3;               % Current-voltage scan rate [Vs-1] 
@@ -22,8 +21,6 @@ par = pc(params_filepath);
 
 %% Obtain and plot equilibrium solution
 sol_eq = equilibrate(par, 1);
-% Export equilibrium electron, hole and electrostatic potential densities
-export_solution(['Equilibrium_solution_', output_filename], sol_eq.el, 0)
 
 %% Call function to obtain equilibrium and cyclic voltammogram solutions
 sol_CV = get_CVs(sol_eq.el, light_intensities, Vmax, Vmin, scan_rate, output_filename);
@@ -37,19 +34,26 @@ J = Jstruct.tot(:,1);                   % Total current at left-hand boundary
 eval(['sol_eq_', output_filename, '= sol_eq;'])
 eval(['sol_CV_',output_filename, '= sol_CV;'])
 
-%% Export the solution for the first light intensity at specified voltage
-Vexport = 0.6;
-export_solution(['Solution at Vapp =', num2str(Vexport), ' V ', output_filename], sol_CV.sol(1), Vexport)
-
 %% Plot cyclic voltammograms
 plot_CVs(sol_CV, light_intensities, xlimits, ylimits);
 
 %% Plot energy level diagram at applied bias Vapp for first light intensity
-Vplot = 1.2;
+Vplot = 0;
+% Get corresponding time, TPLOT for VPLOT
 if Vplot >= 0
     tplot = Vplot/scan_rate;
 elseif Vplot < 0
-    tplot = ((2*Vmax)+abs(Vplot))/scan_rate;
+    tplot = ((2*Vmax)+abs(Vplot))/scan_rate; 
 end
+% PLot the energy level diagram at time TPLOT
 dfplot.ELx(sol_CV.sol(1), tplot);
+% PLot the carrier densities at time TPLOT
 dfplot.npx(sol_CV.sol(1), tplot);
+
+%% Exporting the solutions
+% Export equilibrium electron, hole and electrostatic potential densities
+export_solution(['Equilibrium_solution_', output_filename], sol_eq.el, 0)
+
+% Export the solution for the first light intensity at specified voltage
+Vexport = 0.6;
+export_solution(['Solution at Vapp =', num2str(Vexport), ' V ', output_filename], sol_CV.sol(1), Vexport)
