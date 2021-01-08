4c2175299f63e7ddaa6e95ccc6c06db233fb3c2f
pdb13 <p.calado13@imperial.ac.uk>
2021-01-08 13:21:25 +0000

    script

--- Scripts/df_methods_IonMonger_comparison_SR.m
+++ Scripts/df_methods_IonMonger_comparison_SR.m
@@ -9,7 +9,6 @@ soleq_df_im = equilibrate(par_df_im);
 %% Perform JV scan at 1.2 Vs-1
 % For comparison with IonMonger default parameters
 % sol_CV = doCV(sol_ini, light_intensity, V0, Vmax, Vmin, scan_rate, cycles, tpoints)
-
 CV_df_50mVs_df = doCV(soleq_df_im.ion, 1.0398, 0, -1.2, 0, 50e-3, 1, 241);
 CV_df_100mVs_df = doCV(soleq_df_im.ion, 1.0398, 0, -1.2, 0, 100e-3, 1, 241);
 CV_df_200mVs_df = doCV(soleq_df_im.ion, 1.0398, 0, -1.2, 0, 200e-3, 1, 241);
