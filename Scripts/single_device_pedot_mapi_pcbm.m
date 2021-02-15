01e2588b4c6d0614ea6ea0ca0b93fec0cbd15d05
pdb13 <p.calado13@imperial.ac.uk>
2021-02-15 18:07:30 +0000

    Fixed plots in scripts

--- Scripts/single_device_pedot_mapi_pcbm.m
+++ Scripts/single_device_pedot_mapi_pcbm.m
@@ -38,7 +38,7 @@ legend('100 mVs-1', '200 mVs-1', '400 mVs-1')
 
 %% plot the energy level diagram and carrier densities for the tio2 device at
 % 1 V (t= 10s) during the illuminated forward scan
-dfplot.ELnpx(sol_CV_100mVs, 10)
+dfplot.ELnpx(sol_CV_100mVs_pedot_mapi_pcbm, 10)
 %% Save the workspace- this is commented out as the filepath should lead to
 % a folder on your computer. It is not recommended to store large files in
 % the Github repository as they will be cached even when deleted leading to
