344b35ff83b6d4e1d207492b135058bb86e9f943
Ilario Gelmetti <iochesonome@gmail.com>
2021-02-27 12:35:58 +0100

    MaxTimeStep do not depend on t0

--- Core/df.m
+++ Core/df.m
@@ -149,7 +149,7 @@ J = 0;
 
 %% Solver options
 % MaxStep = limit maximum time step size during integration
-options = odeset('MaxStep', par.MaxStepFactor*0.1*abs(par.tmax - par.t0), 'RelTol', par.RelTol, 'AbsTol', par.AbsTol, 'NonNegative', [1,1,1,0]);
+options = odeset('MaxStep', par.MaxStepFactor*0.1*par.tmax, 'RelTol', par.RelTol, 'AbsTol', par.AbsTol, 'NonNegative', [1,1,1,0]);
 
 %% Call solver
 % inputs with '@' are function handles to the subfunctions
