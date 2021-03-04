e4336611f84bba3d9440f183247dc46f649fe828
pdb13 <p.calado13@imperial.ac.uk>
2021-03-04 21:31:32 +0000

    scripts

--- Scripts/df_methods_IonMonger_comparison_bulk.m
+++ Scripts/df_methods_IonMonger_comparison_bulk.m
@@ -21,25 +21,26 @@ dfplot.JtotVapp(sol_CV_10Vs_im, 0);
 ylim([-10e-3,25e-3])
 legend('0.1 Vs-1', '1 Vs-1', '10 Vs-1') 
 
-%% Electrostatic potential during 1 Vs-1 forward scan
+%% Profile plots
+% Electrostatic potential during 1 Vs-1 forward scan
 dfplot.Vx(sol_CV_1Vs_im, [0, 0.2, 0.4, 0.6, 0.8])
 legend
 legend('HTL','interface 1','Active layer','Interface 2','ETL','0 V', '0.2 V', '0.4 V', '0.6 V', '0.8 V')
 
-%% Electronic carrier densities during during 1 Vs-1 forward scan
+% Electronic carrier densities during during 1 Vs-1 forward scan
 dfplot.npx(sol_CV_1Vs_im, [0, 0.2, 0.4, 0.6, 0.8])
 ylim([1e10, 1e19])
 legend('HTL','interface 1','Active layer','Interface 2','ETL',...
     'n, 0 V', 'p, 0 V', 'n, 0.2 V', 'p, 0.2 V', 'n, 0.4 V', 'p, 0.4 V',...
     'n, 0.6 V', 'p, 0.6 V', 'n, 0.8 V', 'p, 0.8 V')
 
-%% Ionic carrier densities during during 1 Vs-1 forward scan
+% Ionic carrier densities during during 1 Vs-1 forward scan
 dfplot.acx(sol_CV_1Vs_im, [0, 0.2, 0.4, 0.6, 0.8])
 xlim([497, 502])
 legend('HTL','interface 1','Active layer','Interface 2','ETL',...
     'a, 0 V', 'a, 0 V', 'a, 0.2 V', 'a, 0.2 V', 'a, 0.4 V', 'a, 0.4 V',...
     'c, 0.6 V', 'c, 0.6 V', 'c, 0.8 V', 'c, 0.8 V')
 
-%% Equilibrium
+% Equilibrium
 dfplot.ELx(soleq_df_im_bulk.ion)
 
---
+++ Scripts/df_methods_IonMonger_params_barrier_investigation.m
@@ -0,0 +1,55 @@
+initialise_df
+
+%% Read in base parameters
+par_df_im_60p = pc('Input_files/IonMonger_default_IR_60p.csv');
+
+%% Get equilibrium solutions
+soleq_df_im_60p = equilibrate(par_df_im_60p);
+
+%% Perform JV scan at 1.2 Vs-1
+% For comparison with IonMonger default parameters
+% sol_CV = doCV(sol_ini, light_intensity, V0, Vmax, Vmin, scan_rate, cycles, tpoints)
+CV_df_50mVs_im_60p = doCV(soleq_df_im_60p.ion, 1.0398, 0, -1.2, 0, 50e-3, 1, 241);
+CV_df_100mVs_im_60p = doCV(soleq_df_im_60p.ion, 1.0398, 0, -1.2, 0, 100e-3, 1, 241);
+CV_df_200mVs_im_60p = doCV(soleq_df_im_60p.ion, 1.0398, 0, -1.2, 0, 200e-3, 1, 241);
+
+%% Read in base parameters
+par_df_im_100p = pc('Input_files/IonMonger_default_IR_100p.csv');
+
+%% Get equilibrium solutions
+soleq_df_im_100p = equilibrate(par_df_im_100p);
+
+%% Perform JV scan at 1.2 Vs-1
+% For comparison with IonMonger default parameters
+% sol_CV = doCV(sol_ini, light_intensity, V0, Vmax, Vmin, scan_rate, cycles, tpoints)
+CV_df_50mVs_im_100p = doCV(soleq_df_im_100p.ion, 1.0398, 0, -1.2, 0, 50e-3, 1, 241);
+CV_df_100mVs_im_100p = doCV(soleq_df_im_100p.ion, 1.0398, 0, -1.2, 0, 100e-3, 1, 241);
+CV_df_200mVs_im_100p = doCV(soleq_df_im_100p.ion, 1.0398, 0, -1.2, 0, 200e-3, 1, 241);
+
+%% CV plots
+dfplot.JtotVapp(CV_df_50mVs_im_60p, 0)
+hold on
+dfplot.JtotVapp(CV_df_100mVs_im_60p, 0)
+hold on
+dfplot.JtotVapp(CV_df_200mVs_im_60p, 0)
+hold on
+
+dfplot.JtotVapp(CV_df_50mVs_im_100p, 0)
+hold on
+dfplot.JtotVapp(CV_df_100mVs_im_100p, 0)
+hold on
+dfplot.JtotVapp(CV_df_200mVs_im_100p, 0)
+
+figure(91)
+hold on
+plotJV_im(sol_light_50mVs);
+plotJV_im(sol_light_100mVs);
+plotJV_im(sol_light_200mVs);
+
+ylim([-10e-3,25e-3])
+xlim([-1.2, 0])
+
+%% legend for comparison
+legend('DF- 50 mVs-1, 60 pnt interface', 'DF- 100 mVs-1, 60 pnt interface', 'DF- 200 mVs-1,  60 pnt interface',...
+   'DF- 50 mVs-1, 100 pnt interface', 'DF- 100 mVs-1, 100 pnt interface', 'DF- 200 mVs-1,  100 pnt interface',...
+'IM- 50 mVs-1', 'IM- 100 mVs-1', 'IM- 200 mVs-1')
