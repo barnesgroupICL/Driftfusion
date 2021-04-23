1c1a50ba19d19a9cc75befd6a6bb6607865574c9
pdb13 <p.calado13@imperial.ac.uk>
2021-04-23 14:42:07 +0100

    Added mirror labels

--- Scripts/df_methods_IonMonger_comparison_IR.m
+++ Scripts/df_methods_IonMonger_comparison_IR.m
@@ -22,17 +22,17 @@ dfplot.JtotVapp(CV_df_200mVs_im, 0)
 hold on
 
 %% Read in base parameters
-par_df_im = pc('Input_files/IonMonger_default_IR_mirror.csv');
+par_df_im_mirror = pc('Input_files/IonMonger_default_IR_mirror.csv');
 
 %% Get equilibrium solutions
-soleq_df_im = equilibrate(par_df_im);
+soleq_df_im_mirror = equilibrate(par_df_im_mirror);
 
 %% Perform JV scan at 1.2 Vs-1
 % For comparison with IonMonger default parameters
 % sol_CV = doCV(sol_ini, light_intensity, V0, Vmax, Vmin, scan_rate, cycles, tpoints)
-CV_df_50mVs_im_mirror = doCV(soleq_df_im.ion, 1.0398, 0, 1.2, 0, 50e-3, 1, 241);
-CV_df_100mVs_im_mirror = doCV(soleq_df_im.ion, 1.0398, 0, 1.2, 0, 100e-3, 1, 241);
-CV_df_200mVs_im_mirror = doCV(soleq_df_im.ion, 1.0398, 0, 1.2, 0, 200e-3, 1, 241);
+CV_df_50mVs_im_mirror = doCV(soleq_df_im_mirror.ion, 1.0398, 0, 1.2, 0, 50e-3, 1, 241);
+CV_df_100mVs_im_mirror = doCV(soleq_df_im_mirror.ion, 1.0398, 0, 1.2, 0, 100e-3, 1, 241);
+CV_df_200mVs_im_mirror = doCV(soleq_df_im_mirror.ion, 1.0398, 0, 1.2, 0, 200e-3, 1, 241);
 
 %% CV plots
 dfplot.JtotVapp_mirror(CV_df_50mVs_im_mirror, 0)
