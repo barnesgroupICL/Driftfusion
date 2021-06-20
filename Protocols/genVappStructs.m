61d2ff90c31cd3134f2d0340909bb6b23407957c
Ilario Gelmetti <iochesonome@gmail.com>
2021-06-20 14:46:39 +0200

    license

--- Protocols/genVappStructs.m
+++ Protocols/genVappStructs.m
@@ -6,7 +6,7 @@ function VappSol = genVappStructs(solini, Vapp_arr, mobseti)
 % Adapted from original code by I. Gelmetti
 %
 %% LICENSE
-% Copyright (C) 2020  Philip Calado, Ilario Gelmetti, and Piers R. F. Barnes
+% Copyright (C) 2021  Philip Calado, Ilario Gelmetti, and Piers R. F. Barnes
 % Imperial College London
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU Affero General Public License as published
