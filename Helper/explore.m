aa9aa62f973be71b628d8dd1ccf19d0f22ae042c
pdb13 <p.calado13@imperial.ac.uk>
2021-08-03 11:17:34 +0100

    switched back to lightonRs for Voc to speed up unit_test

--- Helper/explore.m
+++ Helper/explore.m
@@ -33,7 +33,7 @@ classdef explore
             errorlog = zeros(length(parval1), length(parval2));
 
             j = 1;
-            parfor i = 1:length(parval1)
+            for i = 1:length(parval1)
                 
                 par = par_base;
                 par = explore.helper(par, str1, parval1(i));
@@ -98,8 +98,8 @@ classdef explore
                         % on the variables being altered.
                         soleq = equilibrate(par);
                         
-                        % Perform JV from 0-1.3 V at 50 mVs-1
-                        JV = doJV(soleq.ion, 50e-3, JVpnts, Int, 1, 0, 1.3, 2);
+                        % Perform JV from 0-1.2 V at 50 mVs-1
+                        JV = doJV(soleq.ion, 50e-3, JVpnts, Int, 1, 0, 1.2, 2);
                         
                         % Get J-V stats
                         stats = dfana.JVstats(JV);
@@ -120,8 +120,10 @@ classdef explore
                         
                         % For steady-state open circuit solution
                         % [sol_Voc, Voc] = findVoc(sol_ini, Int, mobseti, x0, x1, tol, plot)
-                        [sol_Voc, Voc_ss] = findVoc(soleq.ion, 1, 1, stats.Voc_f, stats.Voc_f+0.1, 0.01, JVpnts, 0);
-                        
+                        % [sol_Voc, Voc_ss] = findVoc(soleq.ion, 1, 1, stats.Voc_f, stats.Voc_f+0.1, 0.01, JVpnts, 0);
+                        % sol_Rs = lightonRs(sol_ini, int1, stable_time, mobseti, Rs, pnts)
+                        sol_Voc = lightonRs(soleq.ion, 1, -10, 1, 1e6, JVpnts);
+
                         % Write steady-state solutions into temporary
                         % variables
                         Voc_stable(j,:) = dfana.calcVQFL(sol_Voc);
