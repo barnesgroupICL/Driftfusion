41fa5a0beca981414ab7d0185d35f8e6034efdf5
pdb13 <p.calado13@imperial.ac.uk>
2021-08-05 17:00:09 +0100

    removed unused functions/scripts

--- test_vsr.m
+++
@@ -1,6 +0,0 @@
-%par_tio2 = pc('Input_files/spiro_mapi_tio2.csv');
-
-par_tio2 = pc('Input_files/p_n_heterojunction.csv');
-soleq_tio2 = equilibrate(par_tio2);
-sol_CV_100mVs_tio2_ill = doCV(soleq_tio2.el, 1, 0, 1.0, 0, 100e-3, 1, 241);
-compare_rec_flux(sol_CV_100mVs_tio2_dark, 0.02, 1e6, 1);
