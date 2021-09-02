6b15d9e170a09dce0a16f99c12ae0aabc83cd7a2
pdb13 <p.calado13@imperial.ac.uk>
2021-09-02 10:47:58 +0100

    Updated extract IC to allow 2 element vector [start_time, end_time] as input argument

--- Helper/extract_IC.m
+++ Helper/extract_IC.m
@@ -5,7 +5,8 @@ function sol_ic = extract_IC(sol, requested_time)
 % then be used as the initial conditions for a new simulation with DRIFTFUSION. The time
 % array SOL_IC.T is set to zero (i.e. a single time point) and VAPP is
 % calculated from the SOL parameters and REQUESTED_TIME.
-% 
+% Update 02/09/21 REQUESTED_TIME can now be a 2 element vector [START_TIME,
+% END_TIME]
 %% LICENSE
 % Copyright (C) 2020  Philip Calado, Ilario Gelmetti, and Piers R. F. Barnes
 % Imperial College London
@@ -15,8 +16,17 @@ function sol_ic = extract_IC(sol, requested_time)
 % (at your option) any later version.
 % 
 % START CODE
-index = find(sol.t <= requested_time);
-index = index(end);
+if length(requested_time) == 1
+    index = 0;
+    index = find(sol.t <= requested_time);
+    index = index(end);
+elseif length(requested_time) == 2
+    index = [0,0];
+    index_temp1 = find(sol.t <= requested_time(1));
+    index(1) = index_temp1(end);
+    index_temp2 = find(sol.t <= requested_time(2));
+    index(2) = index_temp2(end);
+end
 
 sol_ic = sol;
 sol_ic.u = sol.u(index, :, :);
@@ -25,6 +35,6 @@ sol_ic.u = sol.u(index, :, :);
 Vappt = dfana.calcVapp(sol);
 sol_ic.par.Vapp = Vappt(index);
 % Overwrite time array
-sol_ic.t = 0;
+sol_ic.t = sol.t(index);
 
 end
