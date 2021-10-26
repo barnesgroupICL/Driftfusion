084366931e063e42743aa6105f5bdf357ffc655c
pdb13 <p.calado13@imperial.ac.uk>
2021-10-26 19:34:44 +0100

    stray figure creation

--- Protocols/findVoc.m
+++ Protocols/findVoc.m
@@ -31,7 +31,7 @@ if mobseti == 1
     % Take ratio of electron and ion mobilities in the active layer
     rat_anion = par.mu_n(par.active_layer)/par.mu_a(par.active_layer);
     rat_cation = par.mu_n(par.active_layer)/par.mu_c(par.active_layer);
-    
+
     % If the ratio is infinity (ion mobility set to zero) then set the ratio to
     % zero instead
     if isnan(rat_anion) || isinf(rat_anion)
@@ -41,7 +41,7 @@ if mobseti == 1
     if isnan(rat_cation) || isinf(rat_cation)
         rat_cation = 0;
     end
-    
+
     par.mobseti = 1;           % Ions are accelerated to reach equilibrium
     par.K_a = rat_anion;
     par.K_c = rat_cation;
@@ -94,12 +94,10 @@ yrun = fx0;
 
 fx1 = 10*tol;                 % Set any initial vale > tol
 
-figure(111)
-
 i = 1;
 while abs(fx1) > tol
     disp(['Newton-Raphson iteration', num2str(i)])
-    
+
     %% Scan to new potential x1
     par.V_fun_type = 'sweep';
     par.V_fun_arg(1) = x0;
@@ -110,48 +108,49 @@ while abs(fx1) > tol
     par.t0 = 0;
 
     sol = df(sol, par);
-    
-    %% Stabilised solution    
+
+    %% Stabilised solution
     par.V_fun_type = 'constant';
     par.V_fun_arg(1) = x1;
     par.tmesh_type = 2;
     par.tmax = 1e-3;
     par.t0 = par.tmax/1e4;
-    
+
     sol = df(sol, par);
-      
+
     J = dfana.calcJ(sol);
     fx1 = J.tot(end, end);
-    
+
     % approximation to the gradient
     fgrad = (fx1 - fx0)/(x1-x0);
-    
+
     if plot
+        figure(111)
         xplot = 0:0.01:3.0;
         yplot = fgrad*xplot + fx0- fgrad*x0;
-        
+
         xrun = [xrun, x1];
         yrun = [yrun, fx1];
-        
+
         plot(xrun, yrun, 'o', xplot, yplot);
         xlim([0.6, 1.2]);
         %ylim([-2e-6, 2e-6])
         xlabel('x');
         ylabel('fx');
         grid on;
-        
+
         drawnow update
     end
     % New initial guesses
     x0 = x1;
-    
-    if fgrad ~= 0     
-        x1 = x1 - (fx1/fgrad);   
-    else  
-        x1 = x1 + 0.1;  
+
+    if fgrad ~= 0
+        x1 = x1 - (fx1/fgrad);
+    else
+        x1 = x1 + 0.1;
     end
-    
-    fx0 = fx1;  
+
+    fx0 = fx1;
     i = i+1;
     disp('Complete')
 end
