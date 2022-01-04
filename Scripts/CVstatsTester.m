9032a290f9208d726e09e8c943d6e5d24e62cea6
pdb13 <p.calado13@imperial.ac.uk>
2022-01-04 13:37:00 +0000

    updated CVstatsTester with ion examples for HF factor calculation

--- Scripts/CVstatsTester.m
+++ Scripts/CVstatsTester.m
@@ -10,13 +10,24 @@
 par = pc('Input_files/spiro_mapi_tio2.csv');
 
 %% Obtain equilibrium solution
-sol = equilibrate(par);
+sol_eq = equilibrate(par);
 
 %% Run current-voltage scans for different applied biases
-sol_CV1 = doCV(sol.el, 1, 0.7, 1.0, 0, 100e-3, 3, 241);
-sol_CV2 = doCV(sol.el, 1, 0, 1.0, 0, 100e-3, 3, 241);
-sol_CV3 = doCV(sol.el, 1, 0, 1.0, 0, 100e-3, 1, 241);
-sol_CV4 = doCV(sol.el, 1, 0, 0.9, 0, 100e-3, 3, 241);
+sol_CV1 = doCV(sol_eq.el, 1, 0.7, 1.0, 0, 100e-3, 3, 241);
+sol_CV2 = doCV(sol_eq.ion, 1, 0, 1.0, 0, 1e-3, 1, 241);
+sol_CV3 = doCV(sol_eq.ion, 1, 0, 1.0, 0, 500e-3, 1, 241);
+sol_CV4 = doCV(sol_eq.ion, 1, 0, 0.9, 0, 1000e-3, 1, 241);
+
+%% Plot outputs
+dfplot.JtotVapp(sol_CV1, 0);
+hold on
+dfplot.JtotVapp(sol_CV2, 0);
+hold on
+dfplot.JtotVapp(sol_CV3, 0);
+hold on
+dfplot.JtotVapp(sol_CV4, 0);
+hold off
+ylim([-30e-3, 10e-3])
 
 %% Get statistics
 stats1 = CVstats(sol_CV1)
