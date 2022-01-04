b0e1f7c71e4069dc8d183efd32043e119799f6e8
pdb13 <p.calado13@imperial.ac.uk>
2022-01-04 12:04:38 +0000

    added calc Pin

--- Core/dfana.m
+++ Core/dfana.m
@@ -146,7 +146,20 @@ classdef dfana
             Ecb = dev_ihalf.EA-V;                                 % Conduction band potential
             Evb = dev_ihalf.IP-V;                                 % Valence band potential
         end
-
+        
+        function Pin = calcPin(sol)
+            % Incident optical power density
+            % Note this integrates across the available spectrum 
+            % within AM15.xls 
+            if strcmp(sol.par.optical_model, 'Beer-Lambert')
+                AM15_data = xlsread('AM15.xls');
+                Pin = 1e-3*trapz(AM15_data(:,1), AM15_data(:,2));
+            else
+                warning('No incident photon spectrum available, assuming Pin = 0.1 W cm-2')
+                Pin = 0.1;
+            end
+        end
+        
         function [J, j, x] = calcJ(sol)
             % Current, J and flux, j calculation from continuity equations
 
