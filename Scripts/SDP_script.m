9541ab956112431f4d895c5a9ca7165fd68b43b5
Ilario Gelmetti <iochesonome@gmail.com>
2022-01-12 17:17:13 +0100

    SDP_script rewrite parts

--- Scripts/SDP_script.m
+++ Scripts/SDP_script.m
@@ -1,7 +1,7 @@
-function sdpsol = SDP_script(sol_ini, tdwell_arr, Vjump, bias_source, bias_int, pulse_source, pulse_int, pulse_tmax, pulse_mobile_ions, solver_split_pulse)
+function sdpsol = SDP_script(sol_ini, tdwell_arr, Vjump, bias_source, bias_int, pulse_source, pulse_int, pulse_tmax, pulse_mobile_ions)
 %SDP_SCRIPT - an alternative script for Step-Dwell-Probe simulations.
 %
-% Syntax:  sdpsol = SDP_script(sol_ini, tdwell_arr, Vjump, bias_source, bias_int, pulse_source, pulse_int, pulse_tmax, pulse_mobile_ions, solver_split_pulse)
+% Syntax:  sdpsol = SDP_script(sol_ini, tdwell_arr, Vjump, bias_source, bias_int, pulse_source, pulse_int, pulse_tmax, pulse_mobile_ions)
 %
 % Inputs:
 %   SOL_INI - a single struct created by DF.
@@ -28,11 +28,6 @@ function sdpsol = SDP_script(sol_ini, tdwell_arr, Vjump, bias_source, bias_int, 
 %     in the pulse (probe) step. As in this step we're usually interested in
 %     the electronic behaviour at times when the ions still didn't move, we
 %     can as well freeze the ions.
-%   SOLVER_SPLIT_PULSE - a boolean, whether is ok to solve the pulse
-%     (probe) step breaking the time axis in smaller simulations. It can
-%     help the cases in where the solver breaks when simulating this step.
-%     The only drawback in activating it is that the simulation will take
-%     longer.
 %
 % Outputs:
 %   SDPSOL - a structure, including: Jdk, the current value at the end of the dwell
@@ -41,7 +36,7 @@ function sdpsol = SDP_script(sol_ini, tdwell_arr, Vjump, bias_source, bias_int, 
 %     parameters as supplied in the input arguments.
 %
 % Example:
-%   sdpsol_spiro_01sun = SDP_script(soleq_spiro.ion, [logspace(-8,3,56)], 0.6, 1, 0.1, 2, 5.12, 1e-3, true, true);
+%   sdpsol_spiro_01sun = SDP_script(soleq_spiro.ion, [logspace(-8,3,56)], 0.6, 1, 0.1, 2, 5.12, 1e-3, true);
 %     perform 56 SDP simulations using dwell times from 10 ns to 1000 s,
 %     using a voltage step of 0.6 V, illuminating from the primary light
 %     source (AM15 by default) with intensity of 0.1 sun during the dwell
@@ -49,14 +44,14 @@ function sdpsol = SDP_script(sol_ini, tdwell_arr, Vjump, bias_source, bias_int, 
 %     an intensity of 5.12 as the pulse with a duration of 1 ms, which is
 %     too long for ensuring that the ions didn't move, so that the first
 %     "true" value instructs the solver to freeze the ions during this
-%     step. The second "true" value allows the script to solve the pulse
-%     step in smaller substeps ensuring convergence of the simulation.
+%     step.
 %
-% Other m-files required: df, dfana
-% Subfunctions: none
+% Other m-files required: df, dfana, runDfFourTrunks
+% Subfunctions: runDfLoosenTolAndReduceMaxStepFactor, calcJtrJdk,
+%   lightRampUp, lightConstant
 % MAT-files required: none
 %
-% See also df, SDP_script_ana, export_SDP_results, doSDP.
+% See also df, anasdp, SDP_list_plot, SDP_script_exporter, doSDP, runDfFourTrunks.
 
 %% LICENSE
 % Copyright (C) 2021  Philip Calado, Ilario Gelmetti, and Piers R. F. Barnes
@@ -92,7 +87,7 @@ if bias_int
 end
 
 disp('Initial voltage (and illumination) jump')
-sol_jump = runDf(sol_ini, par_jump);
+sol_jump = runDfLoosenTolAndReduceMaxStepFactor(sol_ini, par_jump);
 
 par_dwell = lightConstant(par_jump, bias_source, bias_int);
 % unset Vjump parameters
@@ -130,74 +125,48 @@ Jtr = zeros(par_pulseKeepOn.tpoints, length(tdwell_arr));
 t_Jtr = zeros(par_pulseKeepOn.tpoints, length(tdwell_arr));
 Jdk = zeros(1,length(tdwell_arr));
 
-startFromSolJump = true;
+firstRun = true;
+brokenRun = true;
 
 for i = 1:length(tdwell_arr)
     disp(['SDP tdwell = ' num2str(tdwell_arr(i))])
-
-    if ~startFromSolJump
+    if ~firstRun
         par_dwell.tmax = tdwell_arr(i) - tdwell_arr(i-1);
-        sol_dwell = runDf(sol_dwell, par_dwell);
-        if size(sol_dwell.u,1) ~= par_dwell.tpoints
-            startFromSolJump = true;
+        sol_dwell = df(sol_dwell, par_dwell);
+        if size(sol_dwell.u,1) == par_dwell.tpoints
+            brokenRun = false;
         end
     end
-    if startFromSolJump
+    if firstRun || brokenRun
         par_dwell.tmax = tdwell_arr(i);
-        sol_dwell = runDf(sol_jump, par_dwell);
-        %to save the dwell solutions (via assignin) we want to keep
-        %startFromSolJump = true, so you'll have to comment out the whole
-        %if/else block where it could be set to true
-        %assignin('base', matlab.lang.makeValidName(['SDP_dwell_' inputname(1) '_int_' num2str(bias_int) '_tdwell_' num2str(tdwell_arr(i))]), sol_dwell)
+        sol_dwell = df(sol_jump, par_dwell);
         if size(sol_dwell.u,1) == par_dwell.tpoints
-            startFromSolJump = false;
-        else
-            sol_dwell = runDfFourTrunks(sol_jump, par_dwell);
-            % this block is for saving the dwell solution
-%             [sol_dwell, sol1, sol2, sol3] = runDfFourTrunks(sol_jump, par_dwell, '');
-%             J1 = dfana.calcJ(sol1);
-%             J2 = dfana.calcJ(sol2);        
-%             J3 = dfana.calcJ(sol3);
-%             J4 = dfana.calcJ(sol_dwell);
-%             Jtot1 = J1.tot(:,end);
-%             Jtot2 = J2.tot(:,end);        
-%             Jtot3 = J3.tot(:,end);
-%             Jtot4 = J4.tot(:,end);
-% 
-%             Jtot_dwell = [Jtot1; Jtot2; Jtot3; Jtot4];
-%             size(Jtot_dwell)
-%             t_Jtot_dwell = [sol1.t'; (sol2.t+sol1.par.tmax)'; (sol3.t+sol1.par.tmax+sol2.par.tmax)'; (sol_dwell.t+sol1.par.tmax+sol2.par.tmax+sol3.par.tmax)'];
-%             size(t_Jtot_dwell)
-%             assignin('base', matlab.lang.makeValidName(['SDP_dwell_Jtot_' inputname(1) '_int_' num2str(bias_int) '_tdwell_' num2str(tdwell_arr(i))]), [t_Jtot_dwell, Jtot_dwell])
-            if size(sol_dwell.u,1) == par_dwell.tpoints
-                startFromSolJump = false;
-            end
+            brokenRun = false;
         end
+    end
+    if brokenRun
+        sol_dwell = runDfFourTrunks(sol_jump, par_dwell);
     end
     
     disp('Pulse turn on')
-    sol_pulseTurnOn = runDf(sol_dwell, par_pulseTurnOn);
+    sol_pulseTurnOn = runDfLoosenTolAndReduceMaxStepFactor(sol_dwell, par_pulseTurnOn);
     
     disp('Pulse keep on')
-    if solver_split_pulse
+    sol_pulseKeepOn = df(sol_pulseTurnOn, par_pulseKeepOn);
+    if size(sol_pulseKeepOn.u,1) ~= par_pulseKeepOn.tpoints
         sol_pulseKeepOn = runDfFourTrunks(sol_pulseTurnOn, par_pulseKeepOn);
-        [Jtr_temp, Jdk(i)] = calcJtrJdk(sol_dwell, sol_pulseKeepOn);
-        t_Jtr_temp = sol_pulseKeepOn.t';
-        % runDfFourTrunks can output a solution with more time points than
-        % requested, but anasdp works only on matrices, not on cells with
-        % arrays of different lengths, so the interpolated output is used
-        if all(size(Jtr_temp) == size(Jtr(:,i)))
-            Jtr(:,i) = Jtr_temp;
-            t_Jtr(:,i) = t_Jtr_temp;
-        else
-            t_Jtr(:,i) = meshgen_t(par_pulseKeepOn);
-            Jtr(:,i) = interp1(t_Jtr_temp, Jtr_temp, t_Jtr(:,i), 'spline');
-        end
+    end
+    [Jtr_temp, Jdk(i)] = calcJtrJdk(sol_dwell, sol_pulseKeepOn);
+    t_Jtr_temp = sol_pulseKeepOn.t';
+    % runDfFourTrunks can output a solution with more time points than
+    % requested, but anasdp works only on matrices, not on cells with
+    % arrays of different lengths, so the interpolated output is used
+    if all(size(Jtr_temp) == size(Jtr(:,i)))
+        Jtr(:,i) = Jtr_temp;
+        t_Jtr(:,i) = t_Jtr_temp;
     else
-        sol_pulseKeepOn = runDf(sol_pulseTurnOn, par_pulseKeepOn);
-        [Jtr(:,i), Jdk(i)] = calcJtrJdk(sol_dwell, sol_pulseKeepOn);
-        t_Jtr(:,i) = sol_pulseKeepOn.t;
-        %assignin('base', matlab.lang.makeValidName(['SDP_pulseKeepOn' inputname(1) '_' num2str(tdwell_arr(i))]), sol_pulseKeepOn)
+        t_Jtr(:,i) = meshgen_t(par_pulseKeepOn);
+        Jtr(:,i) = interp1(t_Jtr_temp, Jtr_temp, t_Jtr(:,i), 'spline');
     end
 end
 
@@ -252,7 +221,7 @@ function par = lightConstant(par, source, int)
     end
 end
 
-function sol_post = runDf(sol_pre, par)
+function sol_post = runDfLoosenTolAndReduceMaxStepFactor(sol_pre, par)
     sol_post = df(sol_pre, par);
     originalMaxStepFactor = par.MaxStepFactor;
     originalRelTol = par.RelTol;
@@ -260,7 +229,6 @@ function sol_post = runDf(sol_pre, par)
     if size(sol_post.u,1) ~= par.tpoints
         % try to force smaller time steps
         par.MaxStepFactor = par.MaxStepFactor/10;
-        %warning(['Driftfusion:' mfilename],...
         disp([mfilename ' - the solver did not succeed, decreasing the maximum time step changing par.MaxStepFactor to ' num2str(par.MaxStepFactor)])
         sol_post = df(sol_pre, par);
     end
@@ -270,7 +238,6 @@ function sol_post = runDf(sol_pre, par)
         par.RelTol = min(previousRelTol*3.5, 1e-2);
         par.AbsTol = min(previousAbsTol*3.5, 1e-5);
         if par.RelTol ~= previousRelTol || par.AbsTol ~= previousAbsTol
-            %warning(['Driftfusion:' mfilename],...
             disp([mfilename ' - the solver did not succeed, loosening the relative tolerance from ' num2str(previousRelTol) ' to ' num2str(par.RelTol) ' and absolute tolerance from ' num2str(previousAbsTol) ' to ' num2str(par.AbsTol)])
             sol_post = df(sol_pre, par);
         end
@@ -278,7 +245,6 @@ function sol_post = runDf(sol_pre, par)
     if size(sol_post.u,1) ~= par.tpoints
         % try to force smaller time steps
         par.MaxStepFactor = par.MaxStepFactor/10;
-        %warning(['Driftfusion:' mfilename],...
         disp([mfilename ' - the solver did not succeed, decreasing the maximum time step changing par.MaxStepFactor to ' num2str(par.MaxStepFactor)])
         sol_post = df(sol_pre, par);
     end
@@ -288,7 +254,6 @@ function sol_post = runDf(sol_pre, par)
         par.RelTol = min(previousRelTol*3.5, 1e-2);
         par.AbsTol = min(previousAbsTol*3.5, 1e-5);
         if par.RelTol ~= previousRelTol || par.AbsTol ~= previousAbsTol
-            %warning(['Driftfusion:' mfilename],...
             disp([mfilename ' - the solver did not succeed, loosening the relative tolerance from ' num2str(previousRelTol) ' to ' num2str(par.RelTol) ' and absolute tolerance from ' num2str(previousAbsTol) ' to ' num2str(par.AbsTol)])
             sol_post = df(sol_pre, par);
         end
@@ -296,7 +261,6 @@ function sol_post = runDf(sol_pre, par)
     if size(sol_post.u,1) ~= par.tpoints
         % try to force smaller time steps
         par.MaxStepFactor = par.MaxStepFactor/10;
-        %warning(['Driftfusion:' mfilename],...
         disp([mfilename ' - the solver did not succeed, decreasing the maximum time step changing par.MaxStepFactor to ' num2str(par.MaxStepFactor)])
         sol_post = df(sol_pre, par);
     end
