ee3ac0003556e566a508940602d267879cfd2444
pdb13 <p.calado13@imperial.ac.uk>
2022-02-18 11:02:25 +0000

    added input arguments

--- my_script.m
+++ my_script.m
@@ -1,22 +1,19 @@
-%% HELLO MANAN!!!
-
 %% Initialize driftfusion
 initialise_df
 
 %% Add parameter file to path 
-par_sio2 = pc('C:\Users\Manan Mehta\Documents\GitHub\Driftfusion\Input_files\pog2');
+par_sio2 = pc('.\Input_files\pog2');
 
 %% Equilibrium solutions
 soleq_sio2 = equilibrate(par_sio2);
- %% Equilibrium solutions
+
+%% Equilibrium solutions
+% JVsol = doJV(sol_ini, JVscan_rate, JVscan_pnts, Intensity, mobseti, Vstart, Vend, option)
 JVsol = doJV(soleq_sio2.ion, 100e-3, 201, 1, 0, 0, 1, 1);
-%testinggithub
-%misv2test
+
 % plot JV scan
 dfplot.JV(JVsol, 1);
 set(gca,'YScale','log')
 
 ylim([-30e-3,10e-3])
 xlim([-0.2, 1.2])
-
-
